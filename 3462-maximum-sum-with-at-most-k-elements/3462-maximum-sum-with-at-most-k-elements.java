25eb986d4055b79481b6a8fcad9098362be8dcc4
Ujjal Roy <royu99099@gmail.com>
2025-02-23 09:53:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3462-maximum-sum-with-at-most-k-elements/3462-maximum-sum-with-at-most-k-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maxSum(int[][] grid, int[] limits, int k) {
+     List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            Arrays.sort(grid[i]);
+            for (int j = grid[i].length - 1; j >= grid[i].length - limits[i]; j--) {
+                list.add(grid[i][j]);
+            }
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        int maxSum = 0;
+        for (int i = 0; i < k; i++) {
+            maxSum += list.get(i);
+        }
+
+        return maxSum;   
+    }
+}
