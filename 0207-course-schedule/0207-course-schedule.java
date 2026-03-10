6598967790e8f084c5624fd87280ccda2e06aae4
Ujjal Roy <royu99099@gmail.com>
2026-03-10 17:02:36 +0530

    Time: 8 ms (29.33%), Space: 47.3 MB (31.43%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int n, int[][] pre) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] p : pre){
+            adj.get(p[1]).add(p[0]);
+        }
+
+        int[] indegree = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int v : adj.get(i)){
+                indegree[v]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        while(!queue.isEmpty()){
+            int u = queue.poll();
+            count++;
+            
+            for(int v : adj.get(u)){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return count == n;
+    }
+}
