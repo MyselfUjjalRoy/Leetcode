0cca8d98e32993425f7c639539f5c7d2a182dddd
Ujjal Roy <royu99099@gmail.com>
2026-03-10 10:52:15 +0530

    Time: 16 ms (58.41%), Space: 63.6 MB (28.54%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -2,40 +2,54 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int V = graph.length;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++){
-            adj.add(new ArrayList<>());
-        }
+        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
 
-        int[] inorder = new int[V];
-        for(int u = 0; u < V; u++){
-            for(int v : graph[u]){
-                adj.get(v).add(u);
-                inorder[u]++;
-            }
-        }
-        
-        Queue<Integer> queue = new LinkedList<>();
         for(int i = 0; i < V; i++){
-            if(inorder[i] == 0){
-                queue.offer(i);
+            int[] edges = graph[i];
+            for(int v : edges){
+                adj.get(i).add(v);
             }
         }
 
-        ArrayList<Integer> safeStates = new ArrayList<>();
-        while(!queue.isEmpty()){
-            int u = queue.poll();
-            safeStates.add(u);
-            for(int v : adj.get(u)){
-                inorder[v]--;
-                if(inorder[v] == 0){
-                    queue.offer(v);
+        boolean[] visited = new boolean[V];
+        boolean[] pathVisited = new boolean[V];
+        boolean[] safe = new boolean[V];
+
+        for(int i = 0; i < V; i++){
+            dfs(i , adj , visited , pathVisited , safe);
+        }
+        ArrayList<Integer> nodes = new ArrayList<>();
+        for(int i = 0; i < V; i++){
+            if(safe[i]){
+                nodes.add(i);
+            }
+        }
+
+        Collections.sort(nodes);
+
+        return nodes;
+    }
+
+    public boolean dfs(int node , ArrayList<ArrayList<Integer>> adj , boolean[] visited , boolean[] pathVisited , boolean[] safe){
+        visited[node] = true;
+        pathVisited[node] = true;
+
+        for(int nei : adj.get(node)){
+            if(!visited[nei]){
+                if(dfs(nei , adj, visited , pathVisited , safe)){
+                    safe[nei] = false;
+                    return true;
                 }
             }
+            else if(pathVisited[nei]){
+                safe[nei] = false;
+                return true;
+            }
         }
 
-        Collections.sort(safeStates);
-        return safeStates;
-        
-        
+        safe[node] = true;
+        pathVisited[node] = false;
+
+        return false;
     }
 }
