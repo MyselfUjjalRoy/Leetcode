58409f11d0489ecae99c8d6babdeb330499d5fda
Ujjal Roy <royu99099@gmail.com>
2026-03-10 19:26:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,55 +1,46 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int V = graph.length;
+
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for(int i = 0; i < V; i++){
+            adj.add(new ArrayList<>());
+        }   
+        
+        int[] inorder = new int[V];
 
         for(int i = 0; i < V; i++){
             int[] edges = graph[i];
             for(int v : edges){
-                adj.get(i).add(v);
+                adj.get(v).add(i);
+                inorder[i]++;
             }
         }
 
-        boolean[] visited = new boolean[V];
-        boolean[] pathVisited = new boolean[V];
-        boolean[] safe = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
 
         for(int i = 0; i < V; i++){
-            dfs(i , adj , visited , pathVisited , safe);
-        }
-        ArrayList<Integer> nodes = new ArrayList<>();
-        for(int i = 0; i < V; i++){
-            if(safe[i]){
-                nodes.add(i);
+            if(inorder[i] == 0){
+                queue.offer(i);
             }
         }
+        
+        ArrayList<Integer> list = new ArrayList<>();
 
-        Collections.sort(nodes);
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            list.add(node);
 
-        return nodes;
-    }
-
-    public boolean dfs(int node , ArrayList<ArrayList<Integer>> adj , boolean[] visited , boolean[] pathVisited , boolean[] safe){
-        visited[node] = true;
-        pathVisited[node] = true;
-
-        for(int nei : adj.get(node)){
-            if(!visited[nei]){
-                if(dfs(nei , adj, visited , pathVisited , safe)){
-                    safe[nei] = false;
-                    return true;
+            for(int nei : adj.get(node)){
+                inorder[nei]--;
+                if(inorder[nei] == 0){
+                    queue.offer(nei);
                 }
             }
-            else if(pathVisited[nei]){
-                safe[nei] = false;
-                return true;
-            }
         }
+        
+        Collections.sort(list);
 
-        safe[node] = true;
-        pathVisited[node] = false;
-
-        return false;
+        return list;
     }
 }
