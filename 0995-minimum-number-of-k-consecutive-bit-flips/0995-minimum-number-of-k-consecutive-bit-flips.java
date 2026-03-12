4ebef754ecb0e2fbb7cc7c6d4551920c8918e895
Ujjal Roy <royu99099@gmail.com>
2026-03-12 11:38:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -1,6 +1,6 @@
 /*
 solution -> CodestoryWithMik
-approach 2 -> just removing the space for isFlipped by adding a marker at the original array, 2 means isFlipped = true
+approach 3 -> using deque to track i-k bit flip 
 
 */
 class Solution {
@@ -8,22 +8,26 @@ class Solution {
         int n = nums.length;
         int flipCountFromPastForCurr_Idx = 0;
        // boolean[] isFlipped = new boolean[n];
+        Deque<Integer> flipQueue = new LinkedList<>();
 
         int flips = 0;
 
         for(int i = 0; i < n; i++){
-            if(i >= k && nums[i - k] == 2){
-                flipCountFromPastForCurr_Idx--;
+            if(i >= k){
+                flipCountFromPastForCurr_Idx -= flipQueue.pollFirst();
             }
 
-            if(flipCountFromPastForCurr_Idx % 2 == nums[i]){
+            if(flipCountFromPastForCurr_Idx % 2 == nums[i]){//means flip occurred
                 if(i + k > n){
                     return -1;
                 }
 
                 flipCountFromPastForCurr_Idx++;
-                nums[i] = 2;
+                flipQueue.addLast(1);
                 flips++;
+            }
+            else{//means no flips
+                flipQueue.addLast(0);
             }
         }
         return flips;
