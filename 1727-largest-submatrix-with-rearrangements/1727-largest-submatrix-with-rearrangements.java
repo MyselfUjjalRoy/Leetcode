25d4ae49142ae514b3fbe4a298532ac1c625c69d
Ujjal Roy <royu99099@gmail.com>
2026-03-17 11:30:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -2,27 +2,33 @@ class Solution {
     public int largestSubmatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[] prevRow = new int[n];
-
+        ArrayList<int[]> prevHeights = new ArrayList<>();
         int maxArea = 0;
 
         for(int r = 0; r < m; r++){
-            int[] currRow = matrix[r].clone();
-            for(int c = 0; c < n; c++){
-                if(currRow[c] != 0){
-                   currRow[c] += prevRow[c];
+            ArrayList<int[]> heights = new ArrayList<>();
+            boolean[] seen = new boolean[n];
+            for(int[] entry : prevHeights){
+                int height = entry[0];
+                int col = entry[1];
+                if(matrix[r][col] == 1){
+                    heights.add(new int[]{height + 1 , col});
+                    seen[col] = true;
                 }
             }
-            int[] sortedRow = currRow.clone();
-            Arrays.sort(sortedRow);
-            
-            
-            for(int i = 0; i < n; i++){
-                int base = n - i;
-                int height = sortedRow[i];
+
+            for(int col = 0; col < n; col++){
+                if(!seen[col] && matrix[r][col] == 1){
+                    heights.add(new int[]{1 , col});
+                }
+            }
+
+            for(int i = 0; i < heights.size(); i++){
+                int base = i + 1;
+                int height = heights.get(i)[0];
                 maxArea = Math.max(maxArea , base * height);
             }
-            prevRow = currRow;
+            prevHeights = heights;
         }
         return maxArea;
     }
