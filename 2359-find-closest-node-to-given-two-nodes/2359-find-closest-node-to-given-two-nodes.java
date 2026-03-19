4d76abff62f392fe3468f4fb7414236a41785472
Ujjal Roy <royu99099@gmail.com>
2026-03-19 11:51:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,45 +1,47 @@
 class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
+        
         int[] dist1 = new int[n];
         int[] dist2 = new int[n];
-
         Arrays.fill(dist1 , Integer.MAX_VALUE);
         Arrays.fill(dist2 , Integer.MAX_VALUE);
+        
+        bfs(edges , node1 , dist1);
+        bfs(edges , node2 , dist2);
 
-        dist1[node1] = 0;
-        dist2[node2] = 0;
-
-        boolean[] visited1 = new boolean[n];
-        boolean[] visited2 = new boolean[n];
-
-
-        dfs(node1 , edges , visited1 , dist1);
-        dfs(node2 , edges , visited2 , dist2);
-
+        int minDistTillNow = Integer.MAX_VALUE;
         int minNode = -1;
-        int minDistanceTillNow = Integer.MAX_VALUE;
 
         for(int i = 0; i < n; i++){
             if(dist1[i] != Integer.MAX_VALUE && dist2[i] != Integer.MAX_VALUE){
                 int maxDist = Math.max(dist1[i] , dist2[i]);
-
-                if(maxDist < minDistanceTillNow){
-                    minDistanceTillNow = maxDist;
+                if(maxDist < minDistTillNow){
+                    minDistTillNow = maxDist;
                     minNode = i;
                 }
             }
         }
         return minNode;
     }
-    public void dfs(int node , int[] edges , boolean[] visited , int[] dist){
-        visited[node] = true;
-        int v = edges[node];
+    public void bfs(int[] edges , int node , int[] dist){
+        int n = edges.length;
+        boolean[] visited = new boolean[n];
         
+        dist[node] = 0;
+        visited[node] = true;
 
-        while(v != -1 && !visited[v]){
-            dist[v] = dist[node] + 1;
-            dfs(v , edges , visited , dist);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+
+        while(!queue.isEmpty()){
+            int u = queue.poll();
+            int to = edges[u];
+            if(to != -1 && !visited[to]){
+                queue.offer(to);
+                dist[to] = dist[u] + 1;
+                visited[to] = true;
+            }
         }
     }
 }
