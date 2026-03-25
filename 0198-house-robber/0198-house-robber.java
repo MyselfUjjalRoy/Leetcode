b9ed71c3b4b399b3da8bcbe9e0407244d6f5a78e
Ujjal Roy <royu99099@gmail.com>
2026-03-25 15:28:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,23 @@
 class Solution {
-    /*
-    bottom up appraoch - codestory with mik
-    t[i] = max stolen money till i
-    */
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] t = new int[n + 1];
 
-        t[0] = 0; // no house : i = 0
-        t[1] = nums[0]; // 1 house : i = 1
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp , -1);
 
-        for(int i = 2; i <= n; i++){
-            int steal = nums[i - 1] + t[i - 2];
-            int skip = t[i - 1];
+        return solve(0 , n - 1 , nums , dp);
+    }
 
-            t[i] = Math.max(steal , skip);
+    public int solve(int i , int n , int[] nums , int[] dp){
+        if(i > n){
+            return 0;
         }
 
-        return t[n];
+        if(dp[i] != -1) return dp[i];
+
+        int steal = nums[i] + solve(i + 2 , n , nums , dp);
+        int skip = solve(i + 1 , n , nums , dp);
+
+        return dp[i] = Math.max(steal , skip);
     }
 }
