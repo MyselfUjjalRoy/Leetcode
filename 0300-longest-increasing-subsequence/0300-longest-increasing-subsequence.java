1520367787f3ab0ac82d46af50dcf27285ef7b2d
Ujjal Roy <royu99099@gmail.com>
2026-03-26 16:28:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,37 +1,18 @@
 class Solution {
-    int n;
-    int[][] t;
     public int lengthOfLIS(int[] nums) {
-        n = nums.length;
-        t = new int[n + 1][n + 1];
+        int maxLis = 1;
+        int n = nums.length;
+        int[] t = new int[n]; // which refers max length of LIS till i
+        Arrays.fill(t , 1);
         
         for(int i = 0; i < n; i++){
-            Arrays.fill(t[i] , -1);
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    t[i] = Math.max(t[i] , 1 + t[j]);
+                    maxLis = Math.max(maxLis , t[i]);
+                }
+            }
         }
-
-        return solve(nums , -1 , 0); // {nums , prevIdx , currIdx}
-    }
-
-    public int solve(int[] nums , int prevIdx , int currIdx){
-        if(currIdx >= n){
-            return 0;
-        }
-
-        if(prevIdx != -1 && t[prevIdx][currIdx] != -1){
-            return t[prevIdx][currIdx];
-        }
-        
-        int take = 0;
-        if(prevIdx == -1 || nums[prevIdx] < nums[currIdx]){
-            take = 1 + solve(nums , currIdx , currIdx + 1);
-        }
-
-        int skip = solve(nums , prevIdx , currIdx + 1);
-
-        if(prevIdx != -1){
-           t[prevIdx][currIdx] = Math.max(take , skip);
-        }
-
-        return Math.max(take , skip);
+        return maxLis;
     }
 }
