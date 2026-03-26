c4fa521ca8d10093890aa9ef4ce7b5d7b8791bd6
Ujjal Roy <royu99099@gmail.com>
2026-03-26 23:26:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,44 +1,55 @@
 class Solution {
-    public int longestStrChain(String words[]) {
-        // code here
-        int n = words.length;
-        Arrays.sort(words , (s1 , s2) -> s1.length() - s2.length()); 
-        
-        int[] t = new int[n];
-        Arrays.fill(t , 1);
-        
-        int maxLis = 1;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(isPredecessor(words[j] , words[i])){
-                    t[i] = Math.max(t[i] , 1 + t[j]);
-                    maxLis = Math.max(maxLis , t[i]);
-                }
-            }
+    int[][] t;
+    int n;
+    public int longestStrChain(String[] words) {
+        n = words.length;
+        Arrays.sort(words , (a , b) -> a.length() - b.length());
+
+        t = new int[n + 1][n + 1];
+        for(int i = 0; i <= n; i++){
+            Arrays.fill(t[i] , -1);
         }
-        
-        return maxLis;
+
+        return lis(words , -1 , 0);
     }
-    
+
+    public int lis(String[] words , int prevIdx , int currIdx){
+        if(currIdx == n){
+            return 0;
+        }
+
+        if(prevIdx != -1 && t[prevIdx][currIdx] != -1){
+            return t[prevIdx][currIdx];
+        }
+
+        int take = 0;
+        if(prevIdx == -1 || isPredecessor(words[prevIdx] , words[currIdx])){
+            take = 1 + lis(words , currIdx , currIdx + 1);
+        }
+
+        int skip = lis(words , prevIdx , currIdx + 1);
+
+        if(prevIdx != -1){
+            t[prevIdx][currIdx] = Math.max(take , skip);
+        }
+
+        return Math.max(take , skip);
+    }
+
     public boolean isPredecessor(String prev , String curr){
         if((curr.length() - prev.length()) != 1){
             return false;
         }
-        
+        int m = prev.length() , n = curr.length();
         int i = 0 , j = 0;
-        
-        while(i < prev.length() && j < curr.length()){
+
+        while(i < m && j < n){
             if(prev.charAt(i) == curr.charAt(j)){
                 i++;
             }
-            
             j++;
         }
-        
-        return i == prev.length();
+
+        return i == m;
     }
 }
-
-
-
