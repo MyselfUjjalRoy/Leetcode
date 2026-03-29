8a3f181daff7f49d099e52e7b5f031c4dff9caa6
Ujjal Roy <royu99099@gmail.com>
2026-03-29 11:47:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        int n = s1.length();
-        for(int i = 0; i < n - 2; i++){
-            if(s1.charAt(i) != s2.charAt(i)){
-                char[] s = s1.toCharArray();
-                char ch = s[i];
-                s[i] = s[i + 2];
-                s[i + 2] = ch;
+        if(s1.length() != s2.length()) return false;
 
-                s1 = new String(s);                 
+        int[] evenFreq1 = new int[26];
+        int[] evenFreq2 = new int[26];
+        int[] oddFreq1 = new int[26];
+        int[] oddFreq2 = new int[26];
+        
+        int n = s1.length();
+        for(int i = 0; i < n; i++){
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+
+            if(i % 2 == 0){
+                evenFreq1[ch1 - 'a']++;
+                evenFreq2[ch2 - 'a']++;
+            }
+            else{
+                oddFreq1[ch1 - 'a']++;
+                oddFreq2[ch2 - 'a']++;
             }
         }
-        return s1.equals(s2);
+
+        return Arrays.equals(evenFreq1 , evenFreq2) && Arrays.equals(oddFreq1 , oddFreq2);
     }
 }
