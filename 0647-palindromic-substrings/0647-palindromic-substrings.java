a6f03142b0ea03b57ac3dfd346e03c518377030f
Ujjal Roy <royu99099@gmail.com>
2026-04-02 00:19:28 +0530

    Time: 5 ms (97.57%), Space: 42.6 MB (96.99%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,30 +1,25 @@
 class Solution {
+    /* approach 4
+    first option -> assume the i th charcter is the center and making odd length pallindrome
+    sencod option -> assume the i th character + (i + 1)th character is the center and making even length pallindrome
+    */
+    int count;
+    
+    public void check(int i , int j , String s , int n){
+        while(i >= 0 && j < n && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+            count++;
+        }
+    }
     public int countSubstrings(String s) {
         int n = s.length();
-        boolean[][] t = new boolean[n + 1][n + 1];
-        /*
-        t[i][j] = true : s[i:j] is a substring where i and j are inclusive indices
-        */
-        int count = 0;
 
-        for(int length = 1; length <= n; length++){
-            for(int i = 0; i + length - 1 < n; i++){
-                int j = i + length - 1;
+        count = 0;
 
-                if(i == j){// means 1 length string
-                    t[i][j] = true;
-                }
-                else if(i + 1 == j){// means 2 length string
-                    t[i][j] = (s.charAt(i) == s.charAt(j));
-                }
-                else{// length > 2 strings
-                    t[i][j] = (s.charAt(i) == s.charAt(j) && t[i+1][j - 1]);
-                }
-
-                if(t[i][j] == true){// means from i to j , I found a valid palindrome string
-                    count++;
-                }
-            }
+        for(int i = 0; i < n; i ++){
+            check(i , i , s , n); // to check odd length pallidrome
+            check(i , i + 1 , s , n);// to check even length pallindrome
         }
 
         return count;
