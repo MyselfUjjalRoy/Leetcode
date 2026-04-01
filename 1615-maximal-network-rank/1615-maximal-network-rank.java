68cb4170fb068756187c10a4ac769f2606cb6214
Ujjal Roy <royu99099@gmail.com>
2026-04-02 00:38:36 +0530

    Time: 5 ms (73.23%), Space: 48 MB (29.72%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        int[] degree = new int[n];
+        boolean[][] connected = new boolean[n][n];
+
+        for(int[] road : roads){
+            int u = road[0];
+            int v = road[1];
+            
+            degree[u]++;
+            degree[v]++;
+
+            connected[u][v] = true;
+            connected[v][u] = true;
+        }
+
+        int maxRank = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int rank_i = degree[i];
+                int rank_j = degree[j];
+                
+                int rank = rank_i + rank_j;
+
+                if(connected[i][j] || connected[j][i]){
+                    rank--;
+                }
+
+                maxRank = Math.max(maxRank , rank);
+            }
+        }
+
+        return maxRank;
+    }
+}
