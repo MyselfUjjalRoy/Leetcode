553aaed14a6b801fbc30d11244389b7f99022b4a
Ujjal Roy <royu99099@gmail.com>
2026-04-01 11:56:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,27 +2,24 @@ class Solution {
     int m , n ;
     int[][] dp;
 
-    public int solve(String word1 , String word2 , int i , int j){
-        if(i == m){
-            return n - j;// so i need to insert char in word1
-        }
-        else if(j == n){
-            return m - i; // so i need to delete some chars from word1
+    public int solve(String word1 , String word2 , int m , int n){
+        if(m == 0 || n == 0){
+            return m + n;
         }
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[m][n] != -1){
+            return dp[m][n];
         }
 
-        if(word1.charAt(i) == word2.charAt(j)){
-            return dp[i][j] = solve(word1 , word2 , i + 1 , j + 1);
+        if(word1.charAt(m - 1) == word2.charAt(n - 1)){
+            return dp[m][n] = solve(word1 , word2 , m - 1 , n - 1);
         }
         else{
-            int insertChar = 1 + solve(word1 , word2 , i , j + 1);
-            int deleteChar = 1 + solve(word1 , word2 , i + 1, j);
-            int replaceChar = 1 + solve(word1 , word2 , i + 1 , j + 1);
+            int insertChar = 1 + solve(word1 , word2 , m , n - 1);
+            int deleteChar = 1 + solve(word1 , word2 , m - 1 , n);
+            int replaceChar = 1 + solve(word1 , word2 , m - 1 , n - 1);
 
-            return dp[i][j] = Math.min(Math.min(insertChar , deleteChar) , replaceChar);
+            return dp[m][n] = Math.min(Math.min(insertChar , deleteChar) , replaceChar);
         }
     }
     public int minDistance(String word1, String word2) {
@@ -35,6 +32,6 @@ class Solution {
             Arrays.fill(dp[i] , -1);
         }
 
-        return solve(word1 , word2 , 0 , 0);
+        return solve(word1 , word2 , m , n);
     }
 }
