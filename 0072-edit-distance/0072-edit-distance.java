86693f8f37051765e34e3586374e14db56a29de3
Ujjal Roy <royu99099@gmail.com>
2026-04-01 12:01:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,37 +1,26 @@
 class Solution {
-    int m , n ;
-    int[][] dp;
-
-    public int solve(String word1 , String word2 , int m , int n){
-        if(m == 0 || n == 0){
-            return m + n;
-        }
-
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
-
-        if(word1.charAt(m - 1) == word2.charAt(n - 1)){
-            return dp[m][n] = solve(word1 , word2 , m - 1 , n - 1);
-        }
-        else{
-            int insertChar = 1 + solve(word1 , word2 , m , n - 1);
-            int deleteChar = 1 + solve(word1 , word2 , m - 1 , n);
-            int replaceChar = 1 + solve(word1 , word2 , m - 1 , n - 1);
-
-            return dp[m][n] = Math.min(Math.min(insertChar , deleteChar) , replaceChar);
-        }
-    }
     public int minDistance(String word1, String word2) {
-        m = word1.length();
-        n = word2.length();
+        int m = word1.length();
+        int n = word2.length();
 
-        dp = new int[m + 1][n + 1];
+        int[][] dp = new int[m + 1][n + 1];
 
         for(int i = 0; i < m + 1; i++){
-            Arrays.fill(dp[i] , -1);
+            for(int j = 0; j < n + 1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = i + j;
+                }
+                else{
+                    if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                    else{
+                        dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j] , dp[i][ j - 1]) , dp[i - 1][j - 1]);
+                    }
+                }
+            }
         }
 
-        return solve(word1 , word2 , m , n);
+        return dp[m][n];
     }
 }
