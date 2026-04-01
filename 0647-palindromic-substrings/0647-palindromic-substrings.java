1362eb9aa7e775065f1bb999e0025a966b9f3339
Ujjal Roy <royu99099@gmail.com>
2026-04-01 23:57:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,39 +1,27 @@
 class Solution {
-    int[][] t;
-    public int checkPalindrome(int i , int j , String s){
-        if(i > j){
-            return 1;
-        }
-
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if(s.charAt(i) == s.charAt(j)){
-            return t[i][j] = checkPalindrome(i + 1 , j - 1 , s);
-        }
-
-        return 0;
-    }
     public int countSubstrings(String s) {
         int n = s.length();
-
-        t = new int[n + 1][n + 1];
-        for(int i = 0; i < n + 1; i++){
-            Arrays.fill(t[i] , -1);
-        }
-
+        boolean[][] t = new boolean[n + 1][n + 1];
         /*
-        t[i] = -1 // not calculated
-        t[i] = 0 // false
-        t[i] = 1 // true
+        t[i][j] = true : s[i:j] is a substring where i and j are inclusive indices
         */
-
         int count = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(checkPalindrome(i , j , s) == 1){
+        for(int length = 1; length <= n; length++){
+            for(int i = 0; i + length - 1 < n; i++){
+                int j = i + length - 1;
+
+                if(i == j){// means 1 length string
+                    t[i][j] = true;
+                }
+                else if(i + 1 == j){// means 2 length string
+                    t[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else{// length > 2 strings
+                    t[i][j] = (s.charAt(i) == s.charAt(j) && t[i+1][j - 1]);
+                }
+
+                if(t[i][j] == true){// means from i to j , I found a valid palindrome string
                     count++;
                 }
             }
