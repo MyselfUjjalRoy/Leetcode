5f8c71a6c5c716df2f211a8e648119bcf1df20a7
Ujjal Roy <royu99099@gmail.com>
2026-04-01 23:27:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,23 +1,39 @@
 class Solution {
-
-    public boolean checkPalindrome(int i , int j , String s){
+    int[][] t;
+    public int checkPalindrome(int i , int j , String s){
         if(i > j){
-            return true;
-        }
-        if(s.charAt(i) == s.charAt(j)){
-            return checkPalindrome(i + 1 , j - 1 , s);
+            return 1;
         }
 
-        return false;
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if(s.charAt(i) == s.charAt(j)){
+            return t[i][j] = checkPalindrome(i + 1 , j - 1 , s);
+        }
+
+        return 0;
     }
     public int countSubstrings(String s) {
         int n = s.length();
+
+        t = new int[n + 1][n + 1];
+        for(int i = 0; i < n + 1; i++){
+            Arrays.fill(t[i] , -1);
+        }
+
+        /*
+        t[i] = -1 // not calculated
+        t[i] = 0 // false
+        t[i] = 1 // true
+        */
 
         int count = 0;
 
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
-                if(checkPalindrome(i , j , s)){
+                if(checkPalindrome(i , j , s) == 1){
                     count++;
                 }
             }
