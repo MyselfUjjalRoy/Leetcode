b09f1c17386457a6ffaa263084e0aaa918e34d53
Ujjal Roy <royu99099@gmail.com>
2026-04-02 10:47:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,32 +1,26 @@
 class Solution {
+    int maxLen;
+    int start;
+    public void check(int i , int j , String s , int n){
+        while(i >= 0 && j < n && s.charAt(i) == s.charAt(j)){
+            if(maxLen < j - i + 1){
+                maxLen = j - i + 1;
+                start = i;
+            }
+            i--;
+            j++;
+        }
+    }
     public String longestPalindrome(String s) {
         int n = s.length();
-        boolean[][] t = new boolean[n + 1][n + 1];
+        maxLen = 0;
+        start = 0;
 
-        int maxLength = 0;
-        int start = 0;
-
-        for(int length = 1; length <= n; length++){
-            for(int i = 0; i + length - 1 < n; i++){
-                int j = i + length - 1;
-
-                if( i == j){ // means 1 length string
-                    t[i][j] = true;
-                }
-                else if(i + 1 == j){ // means 2 length string
-                    t[i][j] = (s.charAt(i) == s.charAt(j));
-                }
-                else{ // length >= 2 string
-                    t[i][j] = (s.charAt(i) == s.charAt(j) && t[i + 1][j - 1]);
-                }
-
-                if(t[i][j] == true && length > maxLength){
-                    maxLength = length;
-                    start = i;
-                }
-            }
+        for(int i = 0; i < s.length(); i++){
+            check(i , i , s , n);
+            check(i , i + 1 , s , n);
         }
 
-        return s.substring(start , start + maxLength);
+        return s.substring(start , start + maxLen);
     }
 }
