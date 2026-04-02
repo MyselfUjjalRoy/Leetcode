6e39688ed1d36ecdaf7a0f872358f4e5de7caa77
Ujjal Roy <royu99099@gmail.com>
2026-04-02 10:41:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,40 +1,32 @@
 class Solution {
-    int[][] t;
-    public int check(int i , int j , String s){
-        if(i >= j){
-            return 1;
-        }
-
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if(s.charAt(i) == s.charAt(j)){
-            return t[i][j] = check(i + 1 , j - 1 , s);
-        }
-        return t[i][j] = 0;
-    }
     public String longestPalindrome(String s) {
         int n = s.length();
-        t = new int[n + 1][n + 1];
-        for(int i = 0; i < n + 1; i++){
-            Arrays.fill(t[i] , -1);
-        }
+        boolean[][] t = new boolean[n + 1][n + 1];
 
-        int maxLen = Integer.MIN_VALUE;
+        int maxLength = 0;
         int start = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(check(i , j , s) == 1){
-                    if(maxLen < j - i + 1){
-                        maxLen = j - i + 1;
-                        start = i;
-                    }
+        for(int length = 1; length <= n; length++){
+            for(int i = 0; i + length - 1 < n; i++){
+                int j = i + length - 1;
+
+                if( i == j){ // means 1 length string
+                    t[i][j] = true;
+                }
+                else if(i + 1 == j){ // means 2 length string
+                    t[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else{ // length >= 2 string
+                    t[i][j] = (s.charAt(i) == s.charAt(j) && t[i + 1][j - 1]);
+                }
+
+                if(t[i][j] == true && length > maxLength){
+                    maxLength = length;
+                    start = i;
                 }
             }
         }
 
-        return s.substring(start , start + maxLen);
+        return s.substring(start , start + maxLength);
     }
 }
