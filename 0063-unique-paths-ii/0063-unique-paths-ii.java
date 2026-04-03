d4e200a077b5c98b942e850c5f7dc3ea401da17d
Ujjal Roy <royu99099@gmail.com>
2026-04-03 11:00:41 +0530

    Time: 0 ms (100%), Space: 43.8 MB (6.55%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,23 +1,36 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-       int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        int dp[][] = new int[m][n];  
-        
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
-
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+    int[][] t;
+    public int solve(int i , int j , int m , int n , int[][] grid){
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != 0){
+            return 0;
         }
 
-        return recursion(m - 1, n - 1, obstacleGrid, dp);
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if((i == m - 1 && j == n - 1)){
+            return 1;
+        }
+
+        int right = solve(i + 1, j , m , n , grid);
+        int bottom = solve(i , j + 1 , m , n , grid);
+
+        return t[i][j] = right + bottom;
     }
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
-    public int recursion(int m, int n, int[][] obstacleGrid, int[][] dp) {
-        if (m == 0 && n == 0) return 1;
-        if (m < 0 || n < 0 || obstacleGrid[m][n] == 1) return 0;
-        if (dp[m][n] != -1) return dp[m][n];
+        if((grid[m - 1][n - 1] == 1) || (grid[0][0] == 1)){
+            return 0;
+        }
 
-        dp[m][n] = recursion(m - 1, n, obstacleGrid, dp) + recursion(m, n - 1, obstacleGrid, dp);
-        return dp[m][n];
+        t = new int[m][n];
+        for(int i = 0; i < m; i++){
+            Arrays.fill(t[i] , -1);
+        }
+
+        return solve(0 , 0 , m , n , grid);
     }
 }
