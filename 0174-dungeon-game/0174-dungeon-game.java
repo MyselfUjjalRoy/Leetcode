240735357a298ae60144173ae3639bac6ba4a1b6
Ujjal Roy <royu99099@gmail.com>
2026-04-03 22:40:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,38 +1,35 @@
 class Solution {
-//Approach-2 (Using Recursion and Memoization) - ACCEPTED
-//T.C : O(m*n)
-//S.C : O(m*n)
-    int m , n;
-    int[][] t;
-    public int solve(int i , int j , int[][] dungeon){
-        if(i >= m || j >= n){
-            return Integer.MAX_VALUE;
-        }
 
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if(i == m - 1 && j == n - 1){
-            return t[i][j] = (dungeon[i][j] > 0) ? 1 : Math.abs(dungeon[i][j]) + 1;
-        }
-        
-        int down = solve(i + 1 , j , dungeon);
-        int right = solve(i , j + 1 , dungeon);
-
-        int res = Math.min(down , right) - dungeon[i][j];
-
-        return t[i][j] = res > 0 ? res : 1;
-    }
+    // pura recursion + memoization ka code chipkaya haii
+    // kyuki jab bottom se top recursion wala code likhte hai tab pura easyy ho jata haiiiiiii , hurray
     public int calculateMinimumHP(int[][] dungeon) {
-        m = dungeon.length;
-        n = dungeon[0].length;
+        int m = dungeon.length;
+        int n = dungeon[0].length;
 
-        t = new int[201][201];
-        for(int i = 0; i < 201; i++){
-            Arrays.fill(t[i] , -1);
+        int[][] t = new int[m + 1][n + 1];
+        // t[i][j] -> minimum health required to reach dungeon[m - 1][n - 1] from dungeon[i][j]
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1){
+                    if(dungeon[i][j] > 0){
+                        t[i][j] = 1;
+                    }
+                    else{
+                        t[i][j] = Math.abs(dungeon[i][j]) + 1;
+                    }
+                }
+                else{
+                    int right = (j + 1 >= n) ? Integer.MAX_VALUE : t[i][j + 1];
+                    int down = (i + 1 >= m)  ? Integer.MAX_VALUE : t[i + 1][j];
+
+                    int res = Math.min(right , down) - dungeon[i][j];
+
+                    t[i][j] = (res > 0) ? res : 1;
+                }
+            }
         }
 
-        return solve(0 , 0 , dungeon);
+        return t[0][0];
     }
 }
