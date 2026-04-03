8483f02fc5b18a61b1e2b7872f010abebc4558fa
Ujjal Roy <royu99099@gmail.com>
2026-04-03 10:36:42 +0530

    Time: 0 ms (100%), Space: 42.1 MB (72.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -9,6 +9,8 @@ class Solution {
             t[0][j] = 1;
         }
 
+       // t[0][0] = 0;  if m = 1 , n = 1 then also ans = 1 ,not 0 , the written condition will be false
+
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 t[i][j] = t[i - 1][j] + t[i][j - 1];
