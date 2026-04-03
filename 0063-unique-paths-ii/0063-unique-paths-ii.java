79a7cc668d9388e526ede3bbd6d69a9f4217eb74
Ujjal Roy <royu99099@gmail.com>
2026-04-03 11:23:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,36 +1,47 @@
 class Solution {
-    int[][] t;
-    public int solve(int i , int j , int m , int n , int[][] grid){
-        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != 0){
-            return 0;
-        }
-
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if((i == m - 1 && j == n - 1)){
-            return 1;
-        }
-
-        int right = solve(i + 1, j , m , n , grid);
-        int bottom = solve(i , j + 1 , m , n , grid);
-
-        return t[i][j] = right + bottom;
-    }
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 
-        if((grid[m - 1][n - 1] == 1) || (grid[0][0] == 1)){
-            return 0;
+        int[][] t = new int[m][n];
+
+        for(int col = 0; col < n; col++){
+            if(col > 0 && grid[0][col - 1] == 1){
+                t[0][col] = 0;
+                grid[0][col] = 1;
+            }
+            else if(grid[0][col] == 1){
+                t[0][col] = 0;
+            }
+            else{
+                t[0][col] = 1;
+            }
         }
 
-        t = new int[m][n];
-        for(int i = 0; i < m; i++){
-            Arrays.fill(t[i] , -1);
+         for(int row = 0; row < m; row++){
+            if(row > 0 && grid[row - 1][0] == 1){
+                t[row][0] = 0;
+                grid[row][0] = 1;
+            }
+            else if(grid[row][0] == 1){
+                t[row][0] = 0;
+            }
+            else{
+                t[row][0] = 1;
+            }
         }
 
-        return solve(0 , 0 , m , n , grid);
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(grid[i][j] == 1){
+                    t[i][j] = 0;
+                }
+                else{
+                    t[i][j] = t[i - 1][j] + t[i][j - 1];
+                }
+            }
+        }
+
+        return t[m - 1][n - 1];
     }
 }
