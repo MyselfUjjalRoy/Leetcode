ef65537ea3061564c50f4843111f12ca8fe7de4f
Ujjal Roy <royu99099@gmail.com>
2026-04-04 10:10:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -1,35 +1,20 @@
-//Approach-1 (Simple simulation with extra space)
+//Approach-2 (Without extra space)
 //T.C : O(l), l = encodedText.length()
-//S.C : O(l), all characters of encodedText in matrix
+//S.C : O(1)
 // T.C : O(l)
-// S.C : O(l)
+// S.C : O(1)
+
+// corner case :- _b_ _ ac , so in the string there can be space at the beginning of the string also so , don't just trim the whole string or , if you see a space then don't go forward , stop iterating the string , this will also be wrong , so be careful , otherwise it is good to go............
 class Solution {
     public String decodeCiphertext(String encodedText, int rows) {
-        if(rows == 1){
-            return encodedText;
-        }
         int n = encodedText.length();
         int cols = n / rows;
-        char[][] mat = new char[rows][cols];
-        
-        int idx = 0;
-
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                mat[i][j] = encodedText.charAt(idx);
-                idx++;
-            }
-        }
 
         StringBuilder originalText = new StringBuilder();
 
-        // read the matrix diagonally to get the original text
         for(int col = 0; col < cols; col++){
-            int i = 0 , j = col;
-            while(i <  rows && j < cols){
-                originalText.append(mat[i][j]);
-                i++;
-                j++;
+            for(int j = col; j < n; j += (cols + 1)){
+                originalText.append(encodedText.charAt(j));
             }
         }
 
