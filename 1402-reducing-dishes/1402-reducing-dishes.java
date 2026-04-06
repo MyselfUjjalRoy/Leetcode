6734dda20a1cae429267df4b47ed375ac1da2ea5
Ujjal Roy <royu99099@gmail.com>
2026-04-07 00:49:30 +0530

    Time: 19 ms (18.28%), Space: 63.4 MB (6.52%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,30 +1,39 @@
+
 class Solution {
-    int n;
-    public int solve(int[] satisfaction , int i , int time , int[][] dp){
-        if(i >= n){
-            return 0;
-        }
-        
-        if(dp[i][time] != -1){
-            return dp[i][time];
-        }
-
-        int include = satisfaction[i] * time + solve(satisfaction , i + 1 , time + 1 , dp);
-
-        int exclude = solve(satisfaction , i + 1 , time , dp);
-
-        return dp[i][time] = Math.max(include , exclude);
-    }
     public int maxSatisfaction(int[] satisfaction) {
-        n = satisfaction.length;
+        int n = satisfaction.length;
         Arrays.sort(satisfaction);
 
-        int[][] dp = new int[n + 1][n + 1];
-
-        for(int i = 0; i < n + 1; i++){
-            Arrays.fill(dp[i] , -1);
+        // dp[i][t] => max value till 0 ... i and time is j currently
+        long[][] dp = new long[501][501];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i] , Integer.MIN_VALUE);
+        }
+        
+        // at time 0, we have no value. i.e. 0 - COOKING NOT YET STARTED
+        for (int i = 0; i < 501; i++) {
+            dp[i][0] = 0;
         }
 
-        return solve(satisfaction , 0 , 1 , dp);
+        dp[0][1] = satisfaction[0] * 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 1; t <= n; t++) {
+                long include = satisfaction[i] * t + dp[i - 1][t - 1];
+                long exclude = 0 + dp[i - 1][t];
+
+                dp[i][t] = Math.max(include, exclude);
+            }
+        }
+
+        //Since the Qn doesn't ask the maximum value at time = n, but
+        //it asks for the maximum value overall at any time. 
+        //So, we check the values for all time for 0....n-1 food i.e. last row depicts 0...n-1 (t[n-1
+        long ans = 0;
+        for (int time = 0; time <= n; time++) {
+            ans = Math.max(ans, dp[n - 1][time]);
+        }
+
+        return (int) ans;
     }
 }
