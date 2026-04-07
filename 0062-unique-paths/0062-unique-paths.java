155f282379ab54ab86567f6513be3ae3b647a0d2
Ujjal Roy <royu99099@gmail.com>
2026-04-07 15:39:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] t = new int[m][n];
+        int[][] dp = new int[m][n];
 
         for(int i = 0; i < m; i++){
-            t[i][0] = 1;
-        }
-        for(int j = 0; j < n; j++){
-            t[0][j] = 1;
+            dp[i][0] = 1;
         }
 
-       // t[0][0] = 0;  if m = 1 , n = 1 then also ans = 1 ,not 0 , the written condition will be false
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
 
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                t[i][j] = t[i - 1][j] + t[i][j - 1];
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        return t[m - 1][n - 1];
+        return dp[m - 1][n - 1];
     }
 }
