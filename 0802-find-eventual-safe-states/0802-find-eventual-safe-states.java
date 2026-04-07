cd8995e439ffb022680de88d9dd746b14a6c41ae
Ujjal Roy <royu99099@gmail.com>
2026-04-07 19:35:53 +0530

    Time: 26 ms (37.49%), Space: 60.5 MB (81.65%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,44 +1,42 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int V = graph.length;
-
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < V; i++){
             adj.add(new ArrayList<>());
-        }   
-        
-        int[] inorder = new int[V];
+        }
+
+        int[] indegree = new int[V];
 
         for(int i = 0; i < V; i++){
             int[] edges = graph[i];
             for(int v : edges){
                 adj.get(v).add(i);
-                inorder[i]++;
+                indegree[i]++;
             }
         }
 
         Queue<Integer> queue = new LinkedList<>();
-
         for(int i = 0; i < V; i++){
-            if(inorder[i] == 0){
+            if(indegree[i] == 0){
                 queue.offer(i);
             }
         }
-        
-        ArrayList<Integer> list = new ArrayList<>();
 
+        ArrayList<Integer> list = new ArrayList<>();
         while(!queue.isEmpty()){
             int node = queue.poll();
             list.add(node);
 
             for(int nei : adj.get(node)){
-                inorder[nei]--;
-                if(inorder[nei] == 0){
+                indegree[nei]--;
+
+                if(indegree[nei] == 0){
                     queue.offer(nei);
                 }
             }
         }
-        
+
         Collections.sort(list);
 
         return list;
