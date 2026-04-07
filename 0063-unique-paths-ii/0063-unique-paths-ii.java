2e8b383a9cc3acaa9e7a73e341344fed7bb06189
Ujjal Roy <royu99099@gmail.com>
2026-04-07 16:02:56 +0530

    Time: 0 ms (100%), Space: 43.9 MB (5.97%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,6 +4,8 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
+        if(grid[0][0] == 1 || grid[m - 1][n -1] == 1) return 0;
+
         dp = new int[m][n];
 
         for(int i = 0; i < m; i++){
@@ -18,7 +20,7 @@ class Solution {
             return 0;
         }
 
-        if(i == 0 || j == 0) return 1;
+        if(i == 0 && j == 0) return 1;
 
         if(dp[i][j] != -1){
             return dp[i][j];
