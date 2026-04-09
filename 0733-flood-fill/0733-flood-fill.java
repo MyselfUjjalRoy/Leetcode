8bc78c04451cc0ba6d86253db1c1c796fb5838f1
Ujjal Roy <royu99099@gmail.com>
2026-04-10 00:48:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
+    int m , n;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        m = image.length;
+        n = image[0].length;
 
-        int originalColor = image[sr][sc];
-        if(originalColor == color) return image;
+        int orgColor = image[sr][sc];
+        if(orgColor == newColor) return image;
 
-        dfs(image , sr , sc , originalColor , color , n , m);
+        dfs(image , sr , sc , newColor , orgColor);
+
         return image;
     }
-    public void dfs(int[][] image , int row , int col , int originalColor, int newColor , int n , int m){
-        if(row < 0 || col < 0 || row >= n || col >= m || image[row][col] != originalColor){
+
+    public void dfs(int[][] image , int r , int c , int newColor , int orgColor){
+        if(r < 0 || c < 0 || r >= m || c >= m || image[r][c] != orgColor){
             return;
         }
-        image[row][col] = newColor;
-        int[] dx = {-1, 0 , 1 , 0};
-        int[] dy = {0 , 1, 0 , -1};
+
+        image[r][c] = newColor;
+
+        int[] dir = {-1 , 0 , 1 , 0 , 0 , -1 , 0 , 1};
 
         for(int d = 0; d < 4; d++){
-            int nRow = row + dx[d];
-            int nCol = col + dy[d];
+            int nR = r + dir[2 * d];
+            int nC = c + dir[2 * d + 1];
 
-            dfs(image , nRow , nCol , originalColor , newColor , n , m);
+            dfs(image , nR , nC , newColor , orgColor);
         }
     }
 }
