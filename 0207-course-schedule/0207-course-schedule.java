d3068ade5a111c5b85172a2044c33426d49377f8
Ujjal Roy <royu99099@gmail.com>
2026-04-11 02:44:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,35 +1,31 @@
 class Solution {
-    public boolean canFinish(int n, int[][] p) {
+    public boolean canFinish(int n, int[][] pre) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
-        
         int[] indegree = new int[n];
 
-        for(int[] e : p){
-            adj.get(e[1]).add(e[0]);
-            indegree[e[0]]++;
+        for(int[] p : pre){
+            adj.get(p[1]).add(p[0])
+            indegree[p[0]]++;
         }
 
-        Queue<Integer>queue = new LinkedList<>();
-
-        for(int i = 0;i < n ; i++){
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < n; i++){
             if(indegree[i] == 0){
                 queue.offer(i);
             }
         }
-
         int count = 0;
-
         while(!queue.isEmpty()){
-            int node = queue.poll();
+            int u = queue.poll();
             count++;
 
-            for(int nei : adj.get(node)){
-                indegree[nei]--;
-                if(indegree[nei] == 0){
-                    queue.offer(nei);
+            for(int v : adj.get(u)){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    queue.offer(v);
                 }
             }
         }
