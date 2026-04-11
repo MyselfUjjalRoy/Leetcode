c39db02c5148c72a1c4909017bd22f04e96272d8
Ujjal Roy <royu99099@gmail.com>
2026-04-12 00:43:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
+        int[] prev = new int[amount + 1];
 
         for(int t = 0; t <= amount; t++){
             if((t % coins[0]) == 0){
-                dp[0][t] = t / coins[0];
+                prev[t] = t / coins[0];
             }
             else{
-                dp[0][t] = (int)1e9;
+                prev[t] = (int)1e9;
             }
         }
 
         for(int i = 1; i < n; i++){
+            int[] curr = new int[amount + 1];
             for(int t = 0; t <= amount; t++){
-                int notTake = dp[i - 1][t];
+                int notTake = prev[t];
 
                 int take = (int)1e9;
-                if(coins[i] <= t) take = 1 + dp[i][t - coins[i]];
+                if(coins[i] <= t) take = 1 + curr[t - coins[i]];
 
-                dp[i][t] = Math.min(take , notTake);
+                curr[t] = Math.min(take , notTake);
             }
+            prev = curr.clone();
         }
 
-        return dp[n - 1][amount] == (int)1e9 ? -1 : dp[n - 1][amount];
+        return prev[amount] == (int)1e9 ? -1 : prev[amount];
     }
 }
