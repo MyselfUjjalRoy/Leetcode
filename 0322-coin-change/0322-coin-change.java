5fe7399ac3ee1469ec4856ffb43e44ed26ba2016
Ujjal Roy <royu99099@gmail.com>
2026-04-12 00:40:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,28 @@
 class Solution {
-    int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        dp = new int[n][amount + 1];
-        
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dp[i] , -1);
+        int[][] dp = new int[n][amount + 1];
+
+        for(int t = 0; t <= amount; t++){
+            if((t % coins[0]) == 0){
+                dp[0][t] = t / coins[0];
+            }
+            else{
+                dp[0][t] = (int)1e9;
+            }
         }
 
-        int ans = solve(n - 1, amount , coins);
+        for(int i = 1; i < n; i++){
+            for(int t = 0; t <= amount; t++){
+                int notTake = dp[i - 1][t];
 
-        return ans == (int)1e9 ? -1 : ans;
-    }
+                int take = (int)1e9;
+                if(coins[i] <= t) take = 1 + dp[i][t - coins[i]];
 
-    public int solve(int idx , int target , int[] coins){
-        if(idx == 0){
-            if((target % coins[idx]) == 0) return target / coins[idx];
-            else return (int)1e9;
+                dp[i][t] = Math.min(take , notTake);
+            }
         }
 
-        if(dp[idx][target] != -1) return dp[idx][target];
-
-        int notTake = 0 + solve(idx - 1 , target , coins);
-        int take = (int)1e9;
-        if(coins[idx] <= target){
-            take = 1 + solve(idx , target - coins[idx] , coins);
-        }
-
-        return dp[idx][target] = Math.min(take , notTake);
+        return dp[n - 1][amount] == (int)1e9 ? -1 : dp[n - 1][amount];
     }
 }
