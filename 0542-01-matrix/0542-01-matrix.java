30b5d8e631279881019ad1f20e3fd3a427bea79d
Ujjal Roy <royu99099@gmail.com>
2026-04-12 16:20:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,11 +1,13 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] dist = new int[n][m];
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] dist = new int[m][n];
         Queue<int[]> queue = new LinkedList<>();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0){
                     queue.offer(new int[]{i , j});
                 }
@@ -14,21 +16,26 @@ class Solution {
                 }
             }
         }
-        int[] dx = {-1 , 0 , 1 , 0};
-        int[] dy = {0 , 1 , 0 , -1};
+
+        int[] dir = {-1 , 0 , 1 , 0 , 0 , -1 , 0 , 1};
         while(!queue.isEmpty()){
             int[] cell = queue.poll();
-            int row = cell[0];
-            int col = cell[1];
+            int r = cell[0];
+            int c = cell[1];
+
             for(int d = 0; d < 4; d++){
-                int nRow = row + dx[d];
-                int nCol = col + dy[d];
-                if(nRow >= 0 && nCol >= 0 && nRow < n && nCol < m && dist[nRow][nCol] == -1){
-                    queue.offer(new int[]{nRow , nCol});
-                    dist[nRow][nCol] = dist[row][col] + 1;
+                int nR = r + dir[2 * d];
+                int nC = c + dir[2 * d + 1];
+
+                if(nR >= 0 && nC >= 0 && nR < m && nC < n && dist[nR][nC] == -1){
+                    queue.offer(new int[]{nR , nC});
+                    dist[nR][nC] = dist[r][c] + 1;
                 }
             }
         }
+
         return dist;
     }
+
+    
 }
