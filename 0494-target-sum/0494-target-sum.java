cdfb3c29ad2acbf22c38fd5490f59c5bab1680e3
Ujjal Roy <royu99099@gmail.com>
2026-04-12 10:44:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,4 @@
 class Solution {
-    int[][] dp;
     public int findTargetSumWays(int[] nums, int diff) {
         int n = nums.length;
         int total = 0;
@@ -7,37 +6,32 @@ class Solution {
             total += num;
         }
 
-        if((total - diff) % 2 != 0  || (total - diff) < 0) return 0;
+        int target = (total - diff) / 2;
 
-        dp = new int[n][((total - diff) / 2) + 1];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dp[i] , -1);
+        int[][] dp = new int[n][target + 1];
+
+        if(nums[0] == 0){
+            dp[0][0] = 2;
         }
-
-        return solve(n - 1 , (total - diff) / 2 , nums);
-    }
-
-    public int solve(int idx , int target , int[] nums){
-        if(idx == 0){
-            if(nums[0] == 0 && target == 0){
-                return 2;
+        else{
+            dp[0][0] = 1;
+            if(nums[0] <= target){
+                dp[0][nums[0]] = 1;
             }
-            else if(nums[0] == target || target == 0){
-                return 1;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int t = 0; t <= target; t++){
+                int skip = dp[i - 1][t];
+                int take = 0;
+                if(nums[i] <= t){
+                    take = dp[i - 1][t - nums[i]];
+                }
+
+                dp[i][t] = take + skip;
             }
-            else return 0;
-        }
-        if(dp[idx][target] != -1){
-            return dp[idx][target];
         }
 
-        int skip = solve(idx - 1 , target, nums);
-        int take = 0;
-
-        if(nums[idx] <= target){
-            take = solve(idx - 1 , target - nums[idx], nums);
-        }
-
-        return dp[idx][target] = take + skip;
+        return dp[n - 1][target];
     }
 }
