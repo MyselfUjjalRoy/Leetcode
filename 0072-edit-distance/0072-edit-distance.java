4529e918e2e6cc7ca4e8be65b282af504e4304cc
Ujjal Roy <royu99099@gmail.com>
2026-04-13 19:52:26 +0530

    Time: 5 ms (69.77%), Space: 47.2 MB (58.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -10,17 +10,15 @@ class Solution {
                 if(i == 0 || j == 0){
                     dp[i][j] = i + j;
                 }
+
+                else if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
                 else{
-                    if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-                        dp[i][j] = dp[i - 1][j - 1];
-                    }
-                    else{
-                        dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j] , dp[i][ j - 1]) , dp[i - 1][j - 1]);
-                    }
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1] ,Math.min( dp[i - 1][j] , dp[i - 1][j - 1]));
                 }
             }
         }
-
         return dp[m][n];
     }
 }
