b0665b5ae75bd0b3dc1dd240d300c6ca784213ed
Ujjal Roy <royu99099@gmail.com>
2026-04-13 19:05:50 +0530

    Time: 18 ms (90.38%), Space: 46.9 MB (77.58%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,29 +1,26 @@
 class Solution {
-
-    // tc : n^2
-    // sc : n^ 2
     public int minInsertions(String s) {
         int n = s.length();
-        int[][] t = new int[n][n];
-        //t[i][j] = min insertions to make s[i .. j] a pallindrome
+
+        int[][] dp = new int[n][n];
 
         for(int i = 0; i < n; i++){
-            t[i][i] = 0;
+            dp[i][i] = 1;
         }
 
-        for(int length = 2; length <= n; length++){
-            for(int i = 0; i + length - 1 < n; i++){
-                int j = i + length - 1;
+        for(int len = 2; len <= n; len++){
+            for(int i = 0; i + len - 1 < n; i++){
+                int j = i + len - 1;
 
                 if(s.charAt(i) == s.charAt(j)){
-                    t[i][j] = t[i + 1][j - 1];
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
                 }
                 else{
-                    t[i][j] = 1 + Math.min(t[i][j - 1] , t[i + 1][j]);
+                    dp[i][j] = Math.max(dp[i + 1][j] , dp[i][j - 1]);
                 }
             }
         }
 
-        return t[0][n - 1];
+        return n - dp[0][n - 1];
     }
 }
