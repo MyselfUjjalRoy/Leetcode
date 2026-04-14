46f5db97ef2adce8dcf5d8657d233d38f5d488ae
Ujjal Roy <royu99099@gmail.com>
2026-04-14 13:26:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,31 +1,26 @@
 class Solution {
-    int[][] dp;
-
-    public int solve(int i, int j, String s, String t) {
-        if (j == 0) return 1;   // empty t
-        if (i == 0) return 0;   // empty s but t not empty
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s.charAt(i - 1) == t.charAt(j - 1)) {
-            dp[i][j] = solve(i - 1, j - 1, s, t) 
-                     + solve(i - 1, j, s, t);
-        } else {
-            dp[i][j] = solve(i - 1, j, s, t);
-        }
-
-        return dp[i][j];
-    }
-
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
 
-        dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[m + 1][n + 1];
+        // dp[i][j] = number of ways to form first j characters of t using first i characters of s.
+
+        for(int i = 0; i < m + 1; i++){
+            dp[i][0] = 1;
         }
 
-        return solve(m, n, s, t);
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 }
