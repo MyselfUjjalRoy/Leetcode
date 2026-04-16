374f56637d261a3d352791afba8b558f9c8e2921
Ujjal Roy <royu99099@gmail.com>
2026-04-16 20:50:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,24 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n + 1][2][k + 1];
+        int[][] after = new int[2][k + 1];
 
         for(int idx = n - 1; idx >= 0; idx--){
+            int[][] curr = new int[2][k + 1];
             for(int buy = 0; buy <= 1; buy++){
                 for(int capacity = 1; capacity <= k; capacity++){
                     if(buy == 1){
-            dp[idx][buy][capacity] = Math.max(-prices[idx] + dp[idx + 1][0][capacity] , 
-            0 + dp[idx + 1][1][capacity]);
+            curr[buy][capacity] = Math.max(-prices[idx] + after[0][capacity] , 
+            0 + after[1][capacity]);
         }
         else{
-            dp[idx][buy][capacity] = Math.max(prices[idx] + dp[idx + 1][1][capacity - 1] , 
-            0 + dp[idx + 1][0][capacity]);
+            curr[buy][capacity] = Math.max(prices[idx] + after[1][capacity - 1] , 
+            0 + after[0][capacity]);
         } 
                 }
             }
+            after = curr.clone();
         }
-        return dp[0][1][k];
+        return after[1][k];
     }
 }
