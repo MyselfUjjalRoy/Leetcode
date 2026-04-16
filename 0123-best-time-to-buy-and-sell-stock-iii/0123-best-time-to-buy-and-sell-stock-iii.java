76089302a33c8cd2995a23b1fb35419f48534220
Ujjal Roy <royu99099@gmail.com>
2026-04-16 17:15:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,32 +1,34 @@
 class Solution {
-    int n;
-    int[][][] dp;
-    public int solve(int idx , int buy , int capacity , int[] prices){
-        if(capacity == 0) return 0;
-        if(idx == n) return 0;
-
-        if(dp[idx][buy][capacity] != -1){
-            return dp[idx][buy][capacity];
-        }
-
-        if(buy == 1){
-            return dp[idx][buy][capacity] = Math.max(-prices[idx] + solve(idx + 1 , 0 , capacity , prices) , 0 + solve(idx + 1 , 1 , capacity , prices));
-        }
-        else{
-            return dp[idx][buy][capacity] = Math.max(prices[idx] + solve(idx + 1 , 1 , capacity - 1 , prices) , 0 + solve(idx + 1 , 0 , capacity , prices));
-        }
-    }
     public int maxProfit(int[] prices) {
-        n = prices.length;
+        int n = prices.length;
+        int[][][] dp = new int[n + 1][2][3];
 
-        dp = new int[n][2][3];
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 2; j++){
-                Arrays.fill(dp[i][j] , -1);
+        for (int i = 0; i < n; i++) { // base case 1 -> if(capacity == 0) return 0;  we can also avoid it because the value is already 0 , but i have written it for my own understanding
+            for (int buy = 0; buy < 2; buy++) {
+                dp[i][buy][0] = 0;
             }
         }
 
-        return solve(0 , 1 , 2 , prices); // 2 -> initial capacity
+        for (int buy = 0; buy < 2; buy++) { // base case 2 -> if(idx == n) return 0;
+            for (int capacity = 0; capacity < 3; capacity++) {
+                dp[n][buy][capacity] = 0;
+            }
+        }
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int capacity = 1; capacity <= 2; capacity++) {
+                    if (buy == 1) {
+                        dp[idx][buy][capacity] = Math.max(-prices[idx] + dp[idx + 1][0][capacity] ,
+                                0 + dp[idx + 1][1][capacity]);
+                    } else { // means buy = 0
+                        dp[idx][buy][capacity] = Math.max(prices[idx] + dp[idx + 1][1][capacity - 1] ,
+                                0 + dp[idx + 1][0][capacity]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 }
