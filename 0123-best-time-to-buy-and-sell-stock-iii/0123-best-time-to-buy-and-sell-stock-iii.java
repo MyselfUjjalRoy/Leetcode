f64df38e4ec32c0999e488ea17299029885f59ed
Ujjal Roy <royu99099@gmail.com>
2026-04-16 16:54:42 +0530

    Time: 126 ms (14.76%), Space: 150.4 MB (6.81%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    int n;
+    int[][][] dp;
+    public int solve(int idx , int buy , int capacity , int[] prices){
+        if(capacity == 0) return 0;
+        if(idx == n) return 0;
+
+        if(dp[idx][buy][capacity] != -1){
+            return dp[idx][buy][capacity];
+        }
+
+        if(buy == 1){
+            return dp[idx][buy][capacity] = Math.max(-prices[idx] + solve(idx + 1 , 0 , capacity , prices) , 0 + solve(idx + 1 , 1 , capacity , prices));
+        }
+        else{
+            return dp[idx][buy][capacity] = Math.max(prices[idx] + solve(idx + 1 , 1 , capacity - 1 , prices) , 0 + solve(idx + 1 , 0 , capacity , prices));
+        }
+    }
+    public int maxProfit(int[] prices) {
+        n = prices.length;
+
+        dp = new int[n][2][3];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < 2; j++){
+                Arrays.fill(dp[i][j] , -1);
+            }
+        }
+
+        return solve(0 , 1 , 2 , prices); // 2 -> initial capacity
+    }
+}
