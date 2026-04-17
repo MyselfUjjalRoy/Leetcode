ed476938cff5eca0a20a1e24218049da023260e6
Ujjal Roy <royu99099@gmail.com>
2026-04-17 11:07:55 +0530

    Time: 102 ms (14.54%), Space: 55.9 MB (11%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,19 +1,7 @@
 class Solution {
-    int[][] t;
     int n;
-    public int longestStrChain(String[] words) {
-        n = words.length;
-        Arrays.sort(words , (a , b) -> a.length() - b.length());
-
-        t = new int[n + 1][n + 1];
-        for(int i = 0; i <= n; i++){
-            Arrays.fill(t[i] , -1);
-        }
-
-        return lis(words , -1 , 0);
-    }
-
-    public int lis(String[] words , int prevIdx , int currIdx){
+    int[][] t;
+    public int solve(int prevIdx , int currIdx , String[] words){
         if(currIdx == n){
             return 0;
         }
@@ -24,23 +12,21 @@ class Solution {
 
         int take = 0;
         if(prevIdx == -1 || isPredecessor(words[prevIdx] , words[currIdx])){
-            take = 1 + lis(words , currIdx , currIdx + 1);
+            take = 1 + solve(currIdx , currIdx + 1 , words);
         }
+        
+        int notTake = solve(prevIdx , currIdx + 1 , words);
 
-        int skip = lis(words , prevIdx , currIdx + 1);
+        if(prevIdx != -1) t[prevIdx][currIdx] = Math.max(take , notTake);
 
-        if(prevIdx != -1){
-            t[prevIdx][currIdx] = Math.max(take , skip);
-        }
-
-        return Math.max(take , skip);
+        return Math.max(take , notTake);
     }
 
     public boolean isPredecessor(String prev , String curr){
-        if((curr.length() - prev.length()) != 1){
-            return false;
-        }
+        if(curr.length() - prev.length() != 1) return false;
+
         int m = prev.length() , n = curr.length();
+
         int i = 0 , j = 0;
 
         while(i < m && j < n){
@@ -50,6 +36,19 @@ class Solution {
             j++;
         }
 
-        return i == m;
+        return i ==m;
+    }
+    public int longestStrChain(String[] words) {
+        n = words.length;
+
+        Arrays.sort(words , (a , b) -> a.length() - b.length());
+
+        t = new int[n + 1][n + 1];
+
+        for(int i = 0; i < n; i++){
+            Arrays.fill(t[i] , -1);
+        }
+
+        return solve(-1 , 0 , words);
     }
 }
