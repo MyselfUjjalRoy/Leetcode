29ef4a6a54bfada7739d0fb7b861784010ef81e7
Ujjal Roy <royu99099@gmail.com>
2026-04-17 10:45:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -5,23 +5,25 @@ class Solution {
         Arrays.sort(nums);
 
         int[] t = new int[n];
-        Arrays.fill(t , 1);
+        Arrays.fill(t, 1);
 
         int[] prevIdx = new int[n];
-        Arrays.fill(prevIdx , -1);
+        Arrays.fill(prevIdx, -1);
 
         int maxLen = 1;
         int lastIndex = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] % nums[j] == 0){
-                    t[i] = 1 + t[j];
-                    prevIdx[i] = j;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (t[i] < t[j] + 1) {
+                        t[i] = 1 + t[j];
+                        prevIdx[i] = j;
+                    }
                 }
             }
 
-            if(maxLen < t[i]){
+            if (maxLen < t[i]) {
                 maxLen = t[i];
                 lastIndex = i;
             }
@@ -29,7 +31,7 @@ class Solution {
 
         List<Integer> list = new ArrayList<>();
 
-        while(lastIndex != -1){
+        while (lastIndex != -1) {
             list.add(nums[lastIndex]);
             lastIndex = prevIdx[lastIndex];
         }
