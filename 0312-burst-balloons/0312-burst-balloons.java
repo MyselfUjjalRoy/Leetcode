ef2ac9da0b67011885f9fcfaf6ecaae7cdee17ab
Ujjal Roy <royu99099@gmail.com>
2026-04-18 17:10:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,36 +1,32 @@
 class Solution {
-    int[][] dp;
-    public int solve(int i , int j , int[] nums){
-        if(i > j) return 0;
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int ans = Integer.MIN_VALUE;
-
-        for(int idx = i; idx <= j; idx++){
-            int cost = nums[i - 1] * nums[idx] * nums[j + 1] + solve(i , idx - 1 , nums) + solve(idx + 1 , j , nums);
-
-            ans = Math.max(ans , cost);
-        }
-
-        return dp[i][j] = ans;
-    }
     public int maxCoins(int[] nums) {
         int n = nums.length;
         int[] newNums = new int[n + 2];
         newNums[0] = newNums[n + 1] = 1;
 
-        for(int i = 1;i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             newNums[i] = nums[i - 1];
         }
 
-        dp = new int[n + 2][n + 2];
-        for(int i = 0; i < n + 2; i++){
-            Arrays.fill(dp[i] , -1);
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int i = n + 1; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j)
+                    continue;
+
+                int ans = Integer.MIN_VALUE;
+
+                for (int idx = i; idx <= j; idx++) {
+                    int cost = newNums[i - 1] * newNums[idx] * newNums[j + 1] + dp[i][idx - 1]
+                        + dp[idx + 1][j];
+
+                    ans = Math.max(ans, cost);
+                }
+                dp[i][j] = ans;
+            }
         }
 
-        return solve(1 , n  , newNums);
+        return dp[1][n];
     }
 }
