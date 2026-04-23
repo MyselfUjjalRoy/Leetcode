e9ad63cde4c5cb2360215f51d18191502dee5e53
Ujjal Roy <royu99099@gmail.com>
2026-04-24 01:38:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][] after = new int[2][3];
-        
+    int n;
+    int[][][] dp;
+    public int solve(int idx , int buy , int capacity , int[] prices){
+        if(idx == n) return 0;
+        if(capacity == 0) return 0;
 
-        
-
-        for (int idx = n - 1; idx >= 0; idx--) {
-            int[][] curr = new int[2][3];
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int capacity = 1; capacity <= 2; capacity++) {
-                    if (buy == 1) {
-                        curr[buy][capacity] = Math.max(-prices[idx] + after[0][capacity] ,
-                                0 + after[1][capacity]);
-                    } else { // means buy = 0
-                        curr[buy][capacity] = Math.max(prices[idx] + after[1][capacity - 1] ,
-                                0 + after[0][capacity]);
-                    }
-                }
-            }
-            after = curr.clone();
+        if(dp[idx][buy][capacity] != -1){
+            return dp[idx][buy][capacity];
         }
 
-        return after[1][2];
+        if(buy == 1){
+            return dp[idx][buy][capacity] = Math.max(-prices[idx] + solve(idx + 1 , 0 , capacity , prices) , solve(idx + 1 , 1 , capacity , prices));
+        }
+        else{
+            return dp[idx][buy][capacity] = Math.max(prices[idx] + solve(idx + 1 , 1 , capacity - 1 , prices) , solve(idx + 1 , 0 , capacity , prices));
+        }
+    }
+    public int maxProfit(int[] prices) {
+        n = prices.length;
+        dp = new int[n][2][3];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < 2; j++){
+                Arrays.fill(dp[i][j] , -1);
+            }
+        }
+
+        return solve(0 , 1 , 2 , prices);
     }
 }
