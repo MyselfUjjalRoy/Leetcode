d14bc1747ba2b37e6638222b77f6af63d5d88bf3
Ujjal Roy <royu99099@gmail.com>
2026-05-02 14:38:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,23 +1,28 @@
 class Solution {
+    int[] t;
     public int solve(int num){
-        if(num == 0) return 0;
+        
+        if(num == 0) return t[num] = 0;
         int remain = solve(num / 10);
         int digit = num % 10;
 
         int digit_check;   
         if(digit == 0 || digit == 1 || digit == 8) digit_check = 0;
         else if(digit == 2 || digit == 5 || digit == 6 || digit == 9) digit_check = 1;
-        else return 2;
+        else return t[num] = 2;
 
         if(remain == 0 && digit_check == 0){
-            return 0;
+            return t[num] = 0;
         }
         else if(remain == 2 || digit_check == 2){
-            return 2;
+            return t[num] = 2;
         }
-        else return 1; // means valid change found        
+        else return t[num] = 1; // means valid change found        
     }
     public int rotatedDigits(int n) {
+        t = new int[n + 1];
+        Arrays.fill(t , -1);
+
         int count = 0;
         for(int i = 1; i <= n; i++){
             if(solve(i) == 1){
