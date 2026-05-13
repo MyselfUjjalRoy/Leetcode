f98aafe58637046b081a4748c147994074c60832
Ujjal Roy <royu99099@gmail.com>
2026-05-13 12:58:43 +0530

    Time: 4 ms (16.26%), Space: 43.6 MB (45.82%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,27 +1,33 @@
 class Solution {
-    // method 1 - using difference array technique
-    // time complexity - O(n + 101)
+    // method 2 - Line sweep algorithm
+    // time complexity - O(n)+O(nlogn)+O(n)
     public int maximumPopulation(int[][] logs) {
-        int[] diff = new int[2051];
+        List<int[]> events = new ArrayList<>();
 
         for(int[] log : logs){
-            int birth = log[0];
-            int death = log[1];
-
-            diff[birth] += 1;
-            diff[death] -= 1;
+            events.add(new int[]{log[0] , +1}); // birth
+            events.add(new int[]{log[1] , -1}); // death
         }
 
-        int maxPopulation = 0;
+        Collections.sort(events , (a , b) ->{
+            if(a[0] == b[0]){
+                return a[1] - b[1]; // -1 should come first because death will happen first
+            }
+            else{
+                return a[0] - b[0];
+            }
+        });
+
         int currPopulation = 0;
+        int maxPopulation = 0;
         int ans = 1950;
 
-        for(int year = 1950; year <= 2050; year++){
-            currPopulation += diff[year];
+        for(int[] e : events){
+            currPopulation += e[1];
 
             if(currPopulation > maxPopulation){
                 maxPopulation = currPopulation;
-                ans = year;
+                ans = e[0];
             }
         }
 
