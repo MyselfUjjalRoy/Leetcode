e028dbc17027646e397c2c63e381db9a3e908350
Ujjal Roy <royu99099@gmail.com>
2026-05-16 23:43:46 +0530

    Time: 10 ms (51.69%), Space: 59.4 MB (54.84%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,41 +10,49 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
+        if(head == null || head.next == null) return head;
 
-        ListNode slow = head, fast = head, prev = null;
-        while (fast != null && fast.next != null) {
-            prev = slow;
+        ListNode mid = getMiddle(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
+
+        return merge(leftSorted , rightSorted);
+    }
+
+    public ListNode getMiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        prev.next = null;
-        ListNode left = sortList(head);
-        ListNode right = sortList(slow);
 
-        return merge(left, right);
+        return slow;
     }
 
-    public ListNode merge(ListNode head1, ListNode head2) {
-        ListNode dummy = new ListNode(-1);
+    public ListNode merge(ListNode l1 , ListNode l2){
+        ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-        while (head1 != null && head2 != null) {
-            if (head1.val <= head2.val) {
-                tail.next = head1;
-                head1 = head1.next;
-            } else {
-                tail.next = head2;
-                head2 = head2.next;
+
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                tail.next = l1;
+                l1 = l1.next;
             }
+            else{
+                tail.next = l2;
+                l2 = l2.next;
+            }
+
             tail = tail.next;
         }
-        if (head1 != null) {
-            tail.next = head1;
-        } else {
-            tail.next = head2;
-        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+
         return dummy.next;
     }
 }
