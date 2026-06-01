acc468cd8b48735b22a53a45e51e6a2dad34ec26
Ujjal Roy <royu99099@gmail.com>
2026-06-01 23:43:17 +0530

    Time: 1 ms (98.29%), Space: 47.7 MB (25%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,21 +1,36 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rowSet = new HashSet<>();
-        Set<Integer> columnSet = new HashSet<>();
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                if(matrix[i][j] == 0){
-                    rowSet.add(i);
-                    columnSet.add(j);
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int col0 = 1;
+
+        for(int i = 0; i < m; i++) {
+
+            if(matrix[i][0] == 0)
+                col0 = 0;
+
+            for(int j = 1; j < n; j++) {
+
+                if(matrix[i][j] == 0) {
+
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                if(rowSet.contains(i) || columnSet.contains(j)){
+
+        for(int i = m - 1; i >= 0; i--) {
+
+            for(int j = n - 1; j >= 1; j--) {
+
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
-                }
             }
+
+            if(col0 == 0)
+                matrix[i][0] = 0;
         }
     }
 }
