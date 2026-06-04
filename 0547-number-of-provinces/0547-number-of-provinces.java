bd8547072525e3b94a4dc7119debfab7dabefe5d
Ujjal Roy <royu99099@gmail.com>
2026-06-04 12:40:38 +0530

    Time: 4 ms (17.39%), Space: 49.1 MB (18.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,55 +1,40 @@
 class Solution {
-    int[] parent;
-    int[] rank;
-    int find(int x){
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    public void union(int u , int v){
-        int pU = find(u);
-        int pV = find(v);
-
-        if(pU == pV) return;
-
-        if(rank[pU] < rank[pV]){
-            parent[pU] = pV;
-        }
-        else if(rank[pU] > rank[pV]){
-            parent[pV] = pU;
-        }
-        else{
-            parent[pV] = pU;
-            rank[pU]++;
-        }
-    }
-
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        
-        parent = new int[n];
-        rank = new int[n];
-
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(isConnected[i][j] == 1){
-                    union(i , j);
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
 
+        boolean[] visited = new boolean[n];
+
         int provinces = 0;
 
         for(int i = 0; i < n; i++){
-            if(parent[i] == i) provinces++;
+            if(!visited[i]){
+                dfs(i , visited , adj);
+                provinces++;
+            }
         }
 
         return provinces;
+    }
+
+    public void dfs(int node , boolean[] visited , ArrayList<ArrayList<Integer>> adj){
+        visited[node] = true;
+
+        for(int nei : adj.get(node)){
+            if(!visited[nei]){
+                dfs(nei , visited , adj);
+            }
+        }
     }
 }
