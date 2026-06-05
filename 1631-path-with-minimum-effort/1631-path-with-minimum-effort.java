46a46f802ddbddb88685dcab58c54c3ee19fa278
Ujjal Roy <royu99099@gmail.com>
2026-06-05 16:43:17 +0530

    Time: 39 ms (85.6%), Space: 48.1 MB (12.36%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -3,27 +3,27 @@ class Solution {
         int rows = heights.length;
         int cols = heights[0].length;
 
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a , b) -> a[0] - b[0]);
+        pq.offer(new int[]{0 , 0 , 0}); // [effort , row , col]
+
         int[][] effort = new int[rows][cols];
         for(int i = 0; i < rows; i++){
             Arrays.fill(effort[i] , Integer.MAX_VALUE);
         }
         effort[0][0] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a , b)-> a[0] - b[0]);
-        pq.offer(new int[]{0 , 0 , 0}); //[effort , row , col]
-
-        int[] dx = {-1 , 0 , 1 , 0};
-        int[] dy = {0 , 1 , 0 , -1};
-
+        int[] dir = {-1 , 0 , 1 , 0 , 0 , -1 , 0 , 1};
         while(!pq.isEmpty()){
-            int[] cell = pq.poll();
-            int currEffort = cell[0] , row = cell[1] , col = cell[2];
+            int[] curr = pq.poll();
+            int currEffort = curr[0];
+            int row = curr[1];
+            int col = curr[2];
 
             if(row == rows - 1 && col == cols - 1) return currEffort;
 
-            for(int i = 0; i < 4; i++){
-                int nRow = row + dx[i];
-                int nCol = col + dy[i];
+            for(int d = 0; d < 4; d++){
+                int nRow = row + dir[2 * d];
+                int nCol = col + dir[2 * d + 1];
 
                 if(nRow >= 0 && nCol >= 0 && nRow < rows && nCol < cols){
                     int diff = Math.abs(heights[row][col] - heights[nRow][nCol]);
@@ -36,6 +36,7 @@ class Solution {
                 }
             }
         }
-        return 0;
+
+        return 0; // will never reach here;
     }
 }
