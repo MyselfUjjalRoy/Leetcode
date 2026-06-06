4375c13df32f8813f8e08590b5dd7d06575af26c
Ujjal Roy <royu99099@gmail.com>
2026-06-06 17:52:05 +0530

    Time: 4 ms (91.89%), Space: 66.1 MB (67.65%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,52 +1,60 @@
 class Solution {
-    public int findParent(int node , int[] parent){
-        if(parent[node] == node) return node;
-        return parent[node] = findParent(parent[node] , parent);
-    }
-    public void unionBySize(int u , int v , int[] parent , int[] size){
-        int pU = findParent(u , parent);
-        int pV = findParent(v , parent);
-
-        if(pU == pV) return;
-
-        if(size[pU] < size[pV]){
-            parent[pU] = pV;
+    class DSU{
+        int[] parent;
+        int[] rank;
+        DSU(int n){
+            parent = new int[n];
+            rank = new int[n];
+            for(int i = 0; i < n; i++){
+                parent[i] = i;
+            }
         }
-        else{
-            parent[pV] = pU;
-            size[pU] = size[pU] + size[pV];
+
+        int find(int node){
+            if(parent[node] == node) return node;
+            return parent[node] = find(parent[node]);
+        }
+
+        boolean union(int u , int v){
+            int pu = find(u);
+            int pv = find(v);
+
+            if(pu == pv) return false;
+
+            if(rank[pu] < rank[pv]){
+                parent[pu] = pv;
+            }
+            else if(rank[pv] < rank[pu]){
+                parent[pv] = pu;
+            }
+            else{
+                parent[pv] = pu;
+                rank[pu]++;
+            }
+
+            return true;
         }
     }
     public int makeConnected(int n, int[][] connections) {
-        int[] parent = new int[n];
-        int[] size = new int[n];
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-            size[i] = 0;
-        }
+        DSU dsu = new DSU(n);
 
-        int extraEdges = 0;
+        int extraEdge = 0;
+        int component = n;
 
         for(int[] conn : connections){
             int u = conn[0];
             int v = conn[1];
 
-            if(findParent(u , parent) == findParent(v , parent)) extraEdges++;
-            else{
-                unionBySize(u , v , parent , size);
+            if(dsu.union(u , v) == false){
+                extraEdge++;
             }
-        }
-
-        int component = 0;
-
-        for(int i = 0; i < n; i++){
-            if(parent[i] == i){
-                component++;
+            else{
+                component--;
             }
         }
 
         int required = component - 1;
 
-        return extraEdges >= required ? required : -1;
+        return extraEdge >= required ? required : -1;
     }
 }
