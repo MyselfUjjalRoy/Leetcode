9d5714c4fa6b343dba9c07f45b771897ffdff87e
Ujjal Roy <royu99099@gmail.com>
2026-06-06 14:09:58 +0530

    Time: 9 ms (79.75%), Space: 46.5 MB (43.94%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,45 +1,45 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         int[][] dist = new int[n][n];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dist[i] , Integer.MAX_VALUE);
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
             dist[i][i] = 0;
         }
 
-        for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
-            dist[u][v] = wt;
-            dist[v][u] = wt;
+        for (int[] edge : edges) {
+            dist[edge[0]][edge[1]] = edge[2];
+            dist[edge[1]][edge[0]] = edge[2];
         }
 
-        for(int via = 0; via < n; via++){
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(dist[i][via] == Integer.MAX_VALUE || dist[via][j] == Integer.MAX_VALUE){
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][via] == Integer.MAX_VALUE || dist[via][j] == Integer.MAX_VALUE) {
                         continue;
+                    } else {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][via] + dist[via][j]);
                     }
-                    dist[i][j] = Math.min(dist[i][j] , dist[i][via] + dist[via][j]);
                 }
             }
         }
-        
-        int countCity = n;
+
+        int maxCity = n;
         int cityNo = -1;
-        for(int city = 0; city < n; city++){
+
+        for (int city = 0; city < n; city++) {
             int count = 0;
-            for(int adjCity = 0; adjCity < n; adjCity++){
-                if(dist[city][adjCity] <= distanceThreshold){
+            for (int adjCity = 0; adjCity < n; adjCity++) {
+                if (dist[city][adjCity] <= distanceThreshold) {
                     count++;
                 }
             }
 
-            if(count <= countCity){
-                countCity = count;
+            if (count <= maxCity) {
+                maxCity = count;
                 cityNo = city;
             }
         }
+
         return cityNo;
     }
 }
