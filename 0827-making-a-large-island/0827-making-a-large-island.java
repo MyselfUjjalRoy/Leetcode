e38f283c8d4ef1bafd58c9253a6f0619d2c439fb
Ujjal Roy <royu99099@gmail.com>
2026-06-07 16:41:23 +0530

    Time: 107 ms (46.09%), Space: 148.5 MB (44.95%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -2,13 +2,12 @@ class Solution {
     class DSU{
         int[] parent;
         int[] size;
-
         DSU(int n){
             parent = new int[n];
             size = new int[n];
             for(int i = 0; i < n; i++){
-                parent[i] = i;
                 size[i] = 1;
+                parent[i] = i;
             }
         }
 
@@ -16,7 +15,8 @@ class Solution {
             if(parent[node] == node) return node;
             return parent[node] = find(parent[node]);
         }
-        public void union(int u , int v){
+
+        void union(int u , int v){
             int pu = find(u);
             int pv = find(v);
 
@@ -31,6 +31,7 @@ class Solution {
                 size[pu] += size[pv];
             }
         }
+
         int getSize(int node){
             return size[find(node)];
         }
@@ -39,21 +40,20 @@ class Solution {
         int n = grid.length;
         DSU dsu = new DSU(n * n);
 
-        int[] dr = {-1 , 0 , 1 , 0};
-        int[] dc = {0 , 1  , 0 , -1};
+        int[] dir = {-1 , 0 , 1 , 0 , 0 , -1 , 0 , 1};
 
-        // connect all existing 1's
+        // first -> connect all existing ones
         for(int r = 0; r < n; r++){
             for(int c = 0; c < n; c++){
                 if(grid[r][c] == 0) continue;
 
                 for(int d = 0; d < 4; d++){
-                    int nr = r + dr[d];
-                    int nc = c + dc[d];
+                    int nR = r + dir[2 * d];
+                    int nC = c + dir[2 * d + 1];
 
-                    if(isValid(nr , nc , n) && grid[nr][nc] == 1){
+                    if(isValid(nR , nC , n) && grid[nR][nC] == 1){
                         int node = r * n + c;
-                        int adjNode = nr * n + nc;
+                        int adjNode = nR * n + nC;
 
                         dsu.union(node , adjNode);
                     }
@@ -63,7 +63,8 @@ class Solution {
 
         int maxIsland = 0;
 
-        // step 2: try converting each 0 -> 1
+        // step 2 : try converting each 0 -> 1
+       
         for(int r = 0; r < n; r++){
             for(int c = 0; c < n; c++){
                 if(grid[r][c] == 1) continue;
@@ -71,30 +72,34 @@ class Solution {
                 HashSet<Integer> uniqueParents = new HashSet<>();
 
                 for(int d = 0; d < 4; d++){
-                    int nr = r + dr[d];
-                    int nc = c + dc[d];
+                    int nR = r + dir[2 * d];
+                    int nC = c + dir[2 * d + 1];
 
-                    if(isValid(nr , nc , n) && grid[nr][nc] == 1){
-                        uniqueParents.add(dsu.find(nr * n + nc));
+                    if(isValid(nR , nC , n) &&  grid[nR][nC] == 1){
+                        uniqueParents.add(dsu.find(nR * n + nC));
                     }
-                }
+                }   
                 
-                int newSize = 1; // including the flipped cell
+                int newSize = 1; // Including the flipped cell
 
                 for(int parent : uniqueParents){
                     newSize += dsu.size[parent];
                 }
+
                 maxIsland = Math.max(maxIsland , newSize);
             }
         }
 
-        // step 3 : Handle case when grid has no 0 then maxIsand will be 0
-        for(int i = 0; i < n * n; i++){
+        // step 3 : Handle case when grid has no 0 then maxisland will be 0
+        // so to avoid that , we need to take the max existing island without flipping
+
+        for(int i = 0; i < n; i++){
             maxIsland = Math.max(maxIsland , dsu.getSize(i));
         }
 
         return maxIsland;
     }
+
     public boolean isValid(int r , int c , int n){
         return r >= 0 && c >= 0 && r < n && c < n;
     }
