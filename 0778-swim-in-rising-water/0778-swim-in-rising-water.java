d0234d534b41b1e587d5d3972c25523799b08e11
Ujjal Roy <royu99099@gmail.com>
2026-06-08 00:48:35 +0530

    Time: 3 ms (95.94%), Space: 46.3 MB (59.9%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,28 +1,29 @@
 class Solution {
-    int n;
     boolean[][] visited;
-    int[][] directions = {{-1 , 0} , {0 , 1} , {1 , 0} , {0 , -1}};
+    int n;
+    int[] dir = {-1 , 0 , 1 , 0 , 0 , -1 , 0 , 1};
     public int swimInWater(int[][] grid) {
         n = grid.length;
-        int l = 0 , r = n * n - 1;
+        int low = 0;
+        int high = n * n - 1;
 
-        int ans = 0;
-
-        while(l <= r){
-            int mid = l + (r - l) / 2;
+        int ans = Integer.MAX_VALUE;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
             visited = new boolean[n][n];
-
-            if(isReachable(grid , 0 , 0 , mid)){
+            if(isReachable(0 , 0 , mid , grid)){
                 ans = mid;
-                r = mid - 1;
+                high = mid - 1;
             }
-            else {
-                l = mid + 1;
+            else{
+                low = mid + 1;
             }
         }
+
         return ans;
     }
-    public boolean isReachable(int[][] grid , int r , int c , int mid){
+
+    public boolean isReachable(int r , int c , int mid , int[][] grid){
         if(r < 0 || c < 0 || r >= n || c >= n || grid[r][c] > mid || visited[r][c]){
             return false;
         }
@@ -30,15 +31,16 @@ class Solution {
         visited[r][c] = true;
 
         if(r == n - 1 && c == n - 1) return true;
+        
+        for(int d = 0; d < 4; d++){
+            int nr = r + dir[2 * d];
+            int nc = c + dir[2 * d + 1];
 
-        for(int[] dir : directions){
-            int nr = r + dir[0];
-            int nc = c + dir[1];
-
-            if(isReachable(grid , nr , nc , mid)){
+            if(isReachable(nr , nc , mid , grid)){
                 return true;
             }
         }
+
         return false;
     }
 }
