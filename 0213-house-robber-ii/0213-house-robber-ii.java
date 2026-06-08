3539891b0cac5344f80f3b7f4609f13c58b94134
Ujjal Roy <royu99099@gmail.com>
2026-06-08 13:28:55 +0530

    Time: 0 ms (100%), Space: 42.8 MB (39.58%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,44 +1,34 @@
 class Solution {
-    // ekbaar practice kar lete haiiii , revise ho jayega
-    /*
-    at first i will start robbing the houses from 0 to n - 2 because the houses are circular so i can't rob the last house 
-    then secondly i will again start robbing houses from 1 to n - 1 
-    - and between these two whatever maximum value i get , i will return that value
-
-    - this is the same question as house robber , so i will just use the function and call two times
-      and return the answer , that't it you are good to go
-
-    */
     int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
 
-        if(n == 0) return 0;
         if(n == 1) return nums[0];
-
+        
         dp = new int[n];
         Arrays.fill(dp , -1);
 
-        int startFrom_0 = solve(0 , n - 2 , nums);
-        Arrays.fill(dp , - 1);
+        int case1 = solve(0 , n - 1 , nums);
+        
+        Arrays.fill(dp , -1);
 
-        int startFrom_1 = solve(1 , n - 1 , nums);
+        int case2 = solve(1 , n , nums);
 
-        return Math.max(startFrom_0 , startFrom_1);
+        return Math.max(case1 , case2);
     }
 
-    public int solve(int i , int n , int[] nums){
-        if(i > n){
+    public int solve(int idx , int n , int[] nums){
+        if(idx >= n){
             return 0;
         }
 
-        if(dp[i] != -1){
-            return dp[i];
+        if(dp[idx] != -1){
+            return dp[idx];
         }
 
-        int take = nums[i] + solve(i + 2 , n , nums);
-        int skip = solve(i + 1 , n , nums);
+        int skip = solve(idx + 1 , n , nums);
+        int steal = nums[idx] + solve(idx + 2 , n , nums);
 
-        return dp[i] = Math.max(take , skip);
+        return dp[idx] = Math.max(skip , steal);
     }
 }
