c17b8b1bbcb30d2ce0ba1779121649f6a038061d
Ujjal Roy <royu99099@gmail.com>
2026-06-08 12:53:11 +0530

    Time: 0 ms (100%), Space: 43 MB (17.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,18 +2,20 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
-        int[] dp = new int[n + 1];
-        // dp[i] = max stonen money till house i
-        // no house : i = 0
-        dp[0] = 0;
-        dp[1] = nums[0];
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-        for(int i = 2; i <= n; i++){
-            int steal = nums[i - 1] + dp[i - 2];
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int steal = nums[i] + dp[i - 2];
             int skip = dp[i - 1];
-            dp[i] = Math.max(steal , skip);
+            dp[i] = Math.max(steal, skip);
         }
 
-        return dp[n];
+        return dp[n - 1];
     }
 }
