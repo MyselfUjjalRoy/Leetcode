db14f6f1f2f37380071b483bafea02474b11f58e
Ujjal Roy <royu99099@gmail.com>
2026-06-08 12:47:58 +0530

    Time: 0 ms (100%), Space: 42.7 MB (54.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,18 @@
 class Solution {
-    int[] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp , -1);
+        //if(n)
 
-        return solve(0 , n , nums);
-    }
-    public int solve(int idx , int n , int[] nums){
-        if(idx >= n){
-            return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i <= n; i++){
+            int steal = nums[i - 1] + dp[i - 2];
+            int skip = dp[i - 1];
+            dp[i] = Math.max(steal , skip);
         }
 
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-
-        int steal = nums[idx] + solve(idx + 2 , n , nums);
-        int skip = solve(idx + 1 , n , nums);
-
-        return dp[idx] = Math.max(steal , skip);
+        return dp[n];
     }
 }
