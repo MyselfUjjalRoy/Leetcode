30bf9d92fc1247e36dd362d5d68da6241286f996
Ujjal Roy <royu99099@gmail.com>
2026-06-09 13:27:47 +0530

    Time: 200 ms (5.01%), Space: 56.1 MB (11.11%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,54 +1,56 @@
 class Solution {
     int n;
-    int[][] t;
+    int[][] dp;
+    public int longestStrChain(String[] words) {
+        n = words.length;
+        dp = new int[n + 1][n + 1];
+        for(int i = 0; i < n + 1; i++){
+            Arrays.fill(dp[i] , -1);
+        }
+
+        Arrays.sort(words , (a , b) -> a.length() - b.length());
+
+        return solve(-1 , 0 , words);
+    }
+
     public int solve(int prevIdx , int currIdx , String[] words){
-        if(currIdx == n){
+        if(currIdx >= n){
             return 0;
         }
 
-        if(prevIdx != -1 && t[prevIdx][currIdx] != -1){
-            return t[prevIdx][currIdx];
+        if(prevIdx != -1 && dp[prevIdx][currIdx] != -1){
+            return dp[prevIdx][currIdx];
         }
 
         int take = 0;
         if(prevIdx == -1 || isPredecessor(words[prevIdx] , words[currIdx])){
             take = 1 + solve(currIdx , currIdx + 1 , words);
         }
-        
-        int notTake = solve(prevIdx , currIdx + 1 , words);
 
-        if(prevIdx != -1) t[prevIdx][currIdx] = Math.max(take , notTake);
+        int skip = solve(prevIdx , currIdx + 1 , words);
 
-        return Math.max(take , notTake);
+        int ans = Math.max(take , skip);
+
+        if(prevIdx != -1){
+            dp[prevIdx][currIdx] = ans;
+        }
+
+        return ans;
     }
 
-    public boolean isPredecessor(String prev , String curr){
-        if(curr.length() - prev.length() != 1) return false;
-
-        int m = prev.length() , n = curr.length();
+    public boolean isPredecessor(String prevWord , String currWord){
+        if(currWord.length() - prevWord.length() != 1) return false;
 
         int i = 0 , j = 0;
+        int len1 = prevWord.length() , len2 = currWord.length();
 
-        while(i < m && j < n){
-            if(prev.charAt(i) == curr.charAt(j)){
+        while(i < len1 && j < len2){
+            if(prevWord.charAt(i) == currWord.charAt(j)){
                 i++;
             }
             j++;
         }
 
-        return i ==m;
-    }
-    public int longestStrChain(String[] words) {
-        n = words.length;
-
-        Arrays.sort(words , (a , b) -> a.length() - b.length());
-
-        t = new int[n + 1][n + 1];
-
-        for(int i = 0; i < n; i++){
-            Arrays.fill(t[i] , -1);
-        }
-
-        return solve(-1 , 0 , words);
+        return i == len1;
     }
 }
