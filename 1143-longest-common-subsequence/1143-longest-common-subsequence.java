19d62d9ead5ac00550ff7aa230a3dd99d6cb4eef
Ujjal Roy <royu99099@gmail.com>
2026-06-10 12:33:28 +0530

    Time: 65 ms (5.49%), Space: 109.5 MB (5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,43 +1,31 @@
 class Solution {
+    int m , n;
+    int[][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
+        m = text1.length();
+        n = text2.length();
 
-        int[][] dp = new int[m + 1][n + 1];
-
-        for(int i = 1; i < m + 1; i++){
-            for(int j = 1; j < n + 1; j++){
-                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i - 1][j] , dp[i][j - 1]);
-                }
-            }
+        dp = new int[1001][1001];
+        for(int i = 0; i < 1001; i++){
+            Arrays.fill(dp[i] , -1);
         }
 
-        int i = m , j = n;
+        return solve(text1 , text2 , 0 , 0);
+    }
 
-        StringBuilder sb = new StringBuilder();
-        
-        while(i > 0 && j > 0){
-            if(text1.charAt(i - 1) == text2.charAt(j - 1)){
-                sb.append(text1.charAt(i - 1));
-                i--;
-                j--;
-            }
-            else{
-                if(dp[i - 1][j] > dp[i][j - 1]){
-                    i--;
-                }
-                else{
-                    j--;
-                }
-            }
+    public int solve(String text1 , String text2 , int i , int j){
+        if(i >= text1.length() || j >= text2.length()){
+            return 0;
         }
 
-        System.out.println(sb.reverse().toString());
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
 
-        return dp[m][n];
+        if(text1.charAt(i) == text2.charAt(j)){
+            return dp[i][j] = 1 + solve(text1 , text2 , i + 1 , j + 1);
+        }
+
+        return dp[i][j] = Math.max(solve(text1 , text2 , i + 1 , j) , solve(text1 , text2 , i , j + 1));
     }
 }
