261669f0769f4afd482c97ff6e3bc7bbf0051a23
Ujjal Roy <royu99099@gmail.com>
2026-06-10 16:12:03 +0530

    Time: 889 ms (5.05%), Space: 43 MB (41.91%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,25 +1,25 @@
 class Solution {
-    /* approach 4
-    first option -> assume the i th charcter is the center and making odd length pallindrome
-    sencod option -> assume the i th character + (i + 1)th character is the center and making even length pallindrome
-    */
-    int count;
-    
-    public void check(int i , int j , String s , int n){
-        while(i >= 0 && j < n && s.charAt(i) == s.charAt(j)){
-            i--;
-            j++;
-            count++;
+    public boolean checkPallindrome(String s , int i , int j){
+        if(i > j){
+            return true;
         }
+
+        if(s.charAt(i) == s.charAt(j)){
+            return checkPallindrome(s , i + 1 , j - 1);
+        }
+
+        return false;
     }
     public int countSubstrings(String s) {
         int n = s.length();
+        int count = 0;
 
-        count = 0;
-
-        for(int i = 0; i < n; i ++){
-            check(i , i , s , n); // to check odd length pallidrome
-            check(i , i + 1 , s , n);// to check even length pallindrome
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(checkPallindrome(s , i , j)){
+                    count++;
+                }
+            }
         }
 
         return count;
