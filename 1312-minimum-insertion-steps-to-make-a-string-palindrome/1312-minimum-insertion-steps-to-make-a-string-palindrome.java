d29e15112da7cb552f953d7f2e227029adc19f64
Ujjal Roy <royu99099@gmail.com>
2026-06-10 18:23:30 +0530

    Time: 19 ms (85.32%), Space: 47.8 MB (11.04%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,29 +1,25 @@
 class Solution {
     public int minInsertions(String s) {
         int n = s.length();
+
         int[][] dp = new int[n + 1][n + 1];
 
-        // finding length of the Longest pallindromic substring
         for(int length = 1; length <= n; length++){
             for(int i = 0; i + length - 1 < n; i++){
                 int j = i + length - 1;
 
                 if(i == j){
-                    dp[i][j] = 1;
+                    dp[i][j] = 0; // means no chars i need to insert
                 }
                 else if(s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                    dp[i][j] = dp[i + 1][j - 1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i + 1][j] , dp[i][j - 1]);
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j] , dp[i][j - 1]);
                 }
             }
         }
 
-        int lpsLength = dp[0][n - 1];
-
-        int extra = n - lpsLength;
-
-        return extra;
+        return dp[0][n - 1];
     }
 }
