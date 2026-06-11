de358212127cdc0fe7402942118cdf251626173b
Ujjal Roy <royu99099@gmail.com>
2026-06-11 15:50:06 +0530

    Time: 33 ms (51.09%), Space: 257 MB (53.26%) - LeetHub

--- 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.java
+++ 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.java
@@ -6,7 +6,7 @@ class Solution {
         int score = 0;
         for(int i = 0; i < n; i++){
             score += fruits[i][i];
-            fruits[i][i] = 0;
+            //fruits[i][i] = 0;
         }
 
         return score;
