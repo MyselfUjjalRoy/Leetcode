e27901c228f25477e95aec19f5da96912609f743
Ujjal Roy <royu99099@gmail.com>
2026-06-11 11:52:20 +0530

    Time: 0 ms (100%), Space: 42.2 MB (27.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,23 +1,26 @@
 class Solution {
     int[][] dp;
+    int M , N;
     public int uniquePaths(int m, int n) {
         dp = new int[m][n];
+        M = m;
+        N = n;
         for(int i = 0; i < m; i++){
             Arrays.fill(dp[i] , -1);
         }
 
-        return solve(m - 1 , n - 1);
+        return solve(0 , 0);
     }
 
     public int solve(int i , int j){
-        if(i == 0 || j == 0) return 1;
+        if(i == M - 1 || j == N - 1) return 1;
 
-        if(i < 0 || j < 0) return 0;
+        if(i > M - 1 || j > N - 1) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up = solve(i - 1 , j);
-        int left = solve(i , j - 1);
+        int up = solve(i + 1 , j);
+        int left = solve(i , j + 1);
 
         return dp[i][j] = up + left;
     }
