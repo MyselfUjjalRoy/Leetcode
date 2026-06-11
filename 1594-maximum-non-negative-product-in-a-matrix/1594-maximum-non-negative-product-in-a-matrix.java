4be194dfb33b5acbedf3fde573e18d2b417a9a3a
Ujjal Roy <royu99099@gmail.com>
2026-06-11 13:01:36 +0530

    Time: 1 ms (100%), Space: 44.3 MB (31.52%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
@@ -1,61 +1,62 @@
 class Solution {
-    class Pair {
+    class Pair{
         long max , min;
         Pair(long max , long min){
             this.max = max;
             this.min = min;
         }
     }
+
+    Pair[][] dp;
+    int m , n;
+    int MOD = (int)1e9 + 7;
     public int maxProductPath(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int MOD = 1000000007;
+        m = grid.length;
+        n = grid[0].length;
 
-        Pair[][] t = new Pair[m][n];
+        dp = new Pair[m][n];
 
-        t[0][0] = new Pair(grid[0][0] , grid[0][0]);
+        Pair p = solve(0 , 0 , grid);
 
-        // fill first row
-        for(int j = 1; j < n; j++){
-            int val = grid[0][j];
-
-            long a = val * t[0][j - 1].max;
-            long b = val * t[0][j - 1].min;
-
-            t[0][j] = new Pair(Math.max(a , b) , Math.min(a , b));
-        }
-
-        // fill the first column
-        for(int i = 1; i < m; i++){
-            int val = grid[i][0];
-
-            long a = val * t[i - 1][0].max;
-            long b = val * t[i - 1][0].min;
-
-            t[i][0] = new Pair(Math.max(a , b) , Math.min(a , b));
-        }
-
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                int val = grid[i][j];
-
-                long a = t[i - 1][j].max * val;
-                long b = t[i - 1][j].min * val;
-                long c = t[i][j - 1].max * val;
-                long d = t[i][j - 1].min * val;
-
-                long max = Math.max(Math.max(a , b) , Math.max(c , d));
-                long min = Math.min(Math.min(a , b) , Math.min(c , d));
-
-                t[i][j] = new Pair(max , min);
-            }
-        }
-
-        if(t[m - 1][n - 1].max < 0){
+        if(p.max < 0){
             return -1;
         }
         else{
-            return (int)(t[m - 1][n - 1].max % MOD);
+            return (int)(p.max % MOD);
         }
+    }
+
+    public Pair solve(int i , int j , int[][] grid){
+        if(i == m - 1 && j == n - 1){
+            return new Pair(grid[m - 1][n - 1] , grid[m - 1][n - 1]);
+        }
+
+        if(dp[i][j] != null){
+            return dp[i][j];
+        }
+
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
+
+        if(i + 1 < m){
+            Pair down = solve(i + 1 , j , grid);
+
+            long a = down.max * grid[i][j];
+            long b = down.min * grid[i][j];
+
+            max = Math.max(max , Math.max(a , b));
+            min = Math.min(min , Math.min(a , b));
+        }
+        if(j + 1 < n){
+            Pair right = solve(i , j + 1 , grid);
+
+            long a = right.max * grid[i][j];
+            long b = right.min * grid[i][j];
+
+            max = Math.max(max , Math.max(a , b));
+            min = Math.min(min , Math.min(a , b));
+        }
+
+        return dp[i][j] = new Pair(max , min);
     }
 }
