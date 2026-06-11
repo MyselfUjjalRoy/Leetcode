564f7dfb73a2cfd17db5e3f0b52ae02bdf1aa171
Ujjal Roy <royu99099@gmail.com>
2026-06-11 14:50:33 +0530

    Time: 2 ms (70.08%), Space: 45.4 MB (36.19%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,36 +1,31 @@
 class Solution {
-    int m , n;
-    int[][] dp;
     public int calculateMinimumHP(int[][] dungeon) {
-        m = dungeon.length;
-        n = dungeon[0].length;
+        int m = dungeon.length;
+        int n = dungeon[0].length;
 
-        dp = new int[201][201];
-        for(int i = 0; i < 201; i++){
-            Arrays.fill(dp[i] , -1);
+        int[][] dp = new int[m][n];
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1){
+                    if(dungeon[i][j] > 0){
+                        dp[i][j] = 1;
+                    }
+                    else{
+                        dp[i][j] = Math.abs(dungeon[i][j]) + 1;
+                    }
+                }
+                else{
+                    int right = (j + 1 >= n) ? Integer.MAX_VALUE : dp[i][j + 1];
+                    int down = (i + 1 >= m) ? Integer.MAX_VALUE : dp[i + 1][j];
+
+                    int res = Math.min(right , down) - dungeon[i][j];
+
+                    dp[i][j] = res > 0 ? res : 1;
+                }
+            }
         }
 
-        return solve(0 , 0 , dungeon);
-    }
-
-    public int solve(int i , int j , int[][] dungeon){
-        if(i >= m || j >= n){
-            return Integer.MAX_VALUE;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(i == m - 1 && j == n - 1){
-            return dp[i][j] = dungeon[i][j] > 0 ? 1 : Math.abs(dungeon[i][j]) + 1;
-        }
-
-        int down = solve(i + 1 , j , dungeon);
-        int right = solve(i , j + 1 , dungeon);
-
-        int res = Math.min(down , right) - dungeon[i][j];
-
-        return dp[i][j] = res > 0 ? res : 1;
+        return dp[0][0];
     }
 }
