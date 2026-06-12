601ec5ac28964a8f0a1bba3b8e299c76c443e476
Ujjal Roy <royu99099@gmail.com>
2026-06-12 18:34:48 +0530

    Time: 4 ms (68.29%), Space: 94.5 MB (53.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -33,10 +33,11 @@ class Solution {
 
         return true;
     }
+
     public ListNode reverse(ListNode head){
         ListNode prev = null;
         ListNode curr = head;
-
+        
         while(curr != null){
             ListNode next = curr.next;
             curr.next = prev;
