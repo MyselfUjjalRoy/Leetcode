86253bac773b46caf9eff773fa49161a03279d23
Ujjal Roy <royu99099@gmail.com>
2026-06-12 16:54:17 +0530

    Time: 46 ms (5.27%), Space: 48.4 MB (8.18%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,53 +1,50 @@
 class Solution {
-    int m , n;
-    int[][][] dp;
-    int[] colDir = {-1 , 0 , 1};
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
-    public boolean isSafe(int col){
-        return col >= 0 && col < n;
-    }
+        int[][][] dp = new int[m + 1][n + 1][n + 1];
 
-    public int solve(int row , int col1 , int col2 , int[][] grid){
-        if(row >= m){
-            return 0;
-        }
-
-        if(dp[row][col1][col2] != -1){
-            return dp[row][col1][col2];
-        }
-
-        int sum = grid[row][col1];
-        if(col1 != col2){
-            sum += grid[row][col2];
-        }
-
-        int ans = Integer.MIN_VALUE;
-
-        for(int x : colDir){
-            for(int y : colDir){
-                int newCol1 = col1 + x;
-                int newCol2 = col2 + y;
-
-                if(isSafe(newCol1) && isSafe(newCol2)){
-                    ans = Math.max(ans , solve(row + 1 , newCol1 , newCol2 , grid) );
+        for(int j1 = 0; j1 < n; j1++){
+            for(int j2 = 0; j2 < n; j2++){
+                if(j1 == j2){
+                    dp[m - 1][j1][j2] = grid[m - 1][j1];
+                }
+                else{
+                    dp[m - 1][j1][j2] = grid[m - 1][j1] + grid[m - 1][j2];
                 }
             }
         }
 
-        return dp[row][col1][col2] = sum + ans;
-    }
-    public int cherryPickup(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
+        for(int i = m - 2; i >= 0; i--){
+            for(int j1 = 0; j1 < n; j1++){
+                for(int j2 = 0; j2 < n; j2++){
+                    int best = Integer.MIN_VALUE;
+                    for(int dj1 = -1; dj1 <= 1; dj1++){
+                        for(int dj2 = -1; dj2 <= 1; dj2++){
+                            int nj1 = j1 + dj1;
+                            int nj2 = j2 + dj2;
 
-        dp = new int[71][71][71];
+                            if(nj1 >= 0 && nj1 < n && nj2 >= 0 && nj2 < n){
+                                int value = 0;
+                                if(j1 == j2){
+                                    value = grid[i][j1];
+                                }
+                                else{
+                                    value = grid[i][j1] + grid[i][j2];
+                                }
 
-        for(int i = 0; i < 71; i++){
-            for(int j = 0; j < 71; j++){
-                Arrays.fill(dp[i][j] , -1);
+                                value += dp[i + 1][nj1][nj2];
+
+                                best = Math.max(best , value);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = best;
+                }
             }
         }
 
-        return solve(0 , 0 , n - 1 , grid); // [row , col1 , col2 , grid]
+        return dp[0][0][n - 1];
     }
 }
