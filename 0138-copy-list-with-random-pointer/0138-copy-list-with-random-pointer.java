82bca98feb98594ce665e606e20ad8bd25817166
Ujjal Roy <royu99099@gmail.com>
2026-06-14 22:51:27 +0530

    Time: 0 ms (100%), Space: 46.8 MB (13.98%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -13,39 +13,38 @@ class Node {
 }
 */
 
-
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) return null;
-
-        // Step 1: Insert copy nodes
-        Node curr = head;
-        while (curr != null) {
-            Node copy = new Node(curr.val);
-            copy.next = curr.next;
-            curr.next = copy;
-            curr = copy.next;
+        if(head==null){
+            return null;
+        }
+        Node curr=head;
+        while(curr!=null){
+            Node newNode=new Node(curr.val);
+            newNode.next=curr.next;
+            curr.next=newNode;
+            curr=newNode.next;
         }
 
-        // Step 2: Set random pointers
-        curr = head;
-        while (curr != null) {
-            if (curr.random != null) {
-                curr.next.random = curr.random.next;
+        curr=head;
+        while(curr!=null){
+            if(curr.random!=null){
+              curr.next.random=curr.random.next;    
             }
-            curr = curr.next.next;
+            curr=curr.next.next;
         }
 
-        // Step 3: Separate the copied list
-        Node copyHead = head.next;
-        curr = head;
-        while (curr != null) {
-            Node copy = curr.next;
-            curr.next = copy.next;
-            if (copy.next != null) copy.next = copy.next.next;
-            curr = curr.next;
+        curr=head;
+        Node newHead=curr.next;
+        Node newCurr=newHead;
+        while(curr!=null){
+            curr.next=newCurr.next;
+            curr=curr.next;
+            if(curr!=null){
+                newCurr.next=curr.next;
+                newCurr=newCurr.next;
+            }
         }
-
-        return copyHead;
+        return newHead;
     }
 }
