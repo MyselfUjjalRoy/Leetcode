33014b9ad1b71f1ab20806d8b4ffb265d4aec683
Ujjal Roy <royu99099@gmail.com>
2026-06-15 12:37:52 +0530

    Time: 15 ms (81.36%), Space: 46.9 MB (28.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,35 +1,27 @@
 class Solution {
-    int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
 
-        dp = new int[n][amount + 1];
-
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dp[i] , -1);
+        for(int t = 0; t <= amount; t++){
+            if(t % coins[0] == 0) dp[0][t] = t / coins[0];
+            else{
+                dp[0][t] = (int)1e9;
+            }
         }
 
-        int ans = solve(n - 1 , amount , coins);
+        for(int i = 1; i < n; i++){
+            for(int t = 0; t <= amount; t++){
+                int skip = dp[i - 1][t];
+                int take = (int)1e9;
+                if(coins[i] <= t){
+                    take = 1 + dp[i][t - coins[i]];
+                }
 
-        return ans == (int)1e9 ? -1 : ans;
-    }
-
-    public int solve(int idx , int amount , int[] coins){
-        if(idx == 0){
-            if(amount % coins[0] == 0) return amount / coins[0];
-            else return (int)1e9;
+                dp[i][t] = Math.min(take , skip);
+            }
         }
 
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
-        }
-
-        int skip = solve(idx - 1 , amount , coins);
-        int take = (int)1e9; // as we need to find minimum coins
-        if(coins[idx] <= amount){
-            take = 1 + solve(idx , amount - coins[idx] , coins); // Note : idx - 1 nahi karenge , infinite supply
-        }
-
-        return dp[idx][amount] = Math.min(take , skip);
+        return dp[n - 1][amount] == (int)1e9 ? -1 : dp[n - 1][amount];
     }
 }
