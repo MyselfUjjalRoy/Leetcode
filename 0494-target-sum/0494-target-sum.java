078a1ce3943e33b7f462d3998da2ec7652320968
Ujjal Roy <royu99099@gmail.com>
2026-06-15 12:56:29 +0530

    Time: 4 ms (85.74%), Space: 45.1 MB (38.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    int[][] dp;
     public int findTargetSumWays(int[] nums, int diff) {
         int n = nums.length;
         int total = 0;
@@ -8,34 +9,35 @@ class Solution {
 
         if((total - diff) % 2 != 0  || (total - diff) < 0) return 0;
 
-        int target = (total - diff) / 2;
-
-        int[] prev = new int[target + 1];
-
-        if(nums[0] == 0){
-            prev[0] = 2;
+        dp = new int[n][((total - diff) / 2) + 1];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i] , -1);
         }
-        else{
-            prev[0] = 1;
-            if(nums[0] <= target){
-                prev[nums[0]] = 1;
+
+        return solve(n - 1 , (total - diff) / 2 , nums);
+    }
+
+    public int solve(int idx , int target , int[] nums){
+        if(idx == 0){
+            if(nums[0] == 0 && target == 0){
+                return 2;
             }
-        }
-
-        for(int i = 1; i < n; i++){
-            int[] curr = new int[target + 1];
-            for(int t = 0; t <= target; t++){
-                int skip = prev[t];
-                int take = 0;
-                if(nums[i] <= t){
-                    take = prev[t - nums[i]];
-                }
-
-                curr[t] = take + skip;
+            else if(nums[0] == target || target == 0){
+                return 1;
             }
-            prev = curr.clone();
+            else return 0;
+        }
+        if(dp[idx][target] != -1){
+            return dp[idx][target];
         }
 
-        return prev[target];
+        int skip = solve(idx - 1 , target, nums);
+        int take = 0;
+
+        if(nums[idx] <= target){
+            take = solve(idx - 1 , target - nums[idx], nums);
+        }
+
+        return dp[idx][target] = take + skip;
     }
 }
