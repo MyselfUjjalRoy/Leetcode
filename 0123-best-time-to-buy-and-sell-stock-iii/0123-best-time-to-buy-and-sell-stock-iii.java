b40d7b7c372688064df1c49a3134935774673c57
Ujjal Roy <royu99099@gmail.com>
2026-06-18 13:31:02 +0530

    Time: 63 ms (30.18%), Space: 87.9 MB (42.79%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,37 +1,42 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n + 1][2][3];
+        int[][] prev = new int[2][3];
 
         for(int i = 0; i < n + 1; i++){ // base case 2 -> capacity == 0
             for(int option = 0; option <= 1; option++){
-                dp[i][option][0] = 0;
+                prev[option][0] = 0;
             }
         }
 
         for(int option = 0; option <= 1; option++){ // base case 1 -> idx == n
             for(int capacity = 0; capacity <= 2; capacity++){
-                dp[n][option][capacity] = 0;
+                prev[option][capacity] = 0;
             }
         }
 
         for(int idx = n - 1; idx >= 0; idx--){
-            for(int option = 0; option <= 1; option++){
+
+            int[][] curr = new int[2][3];
+
+             for(int option = 0; option <= 1; option++){
                 for(int capacity = 1; capacity <= 2; capacity++){
                     int profit = 0;
 
                     if(option == 1){
-                        profit = Math.max(-prices[idx] + dp[idx + 1][0][capacity] , 0 + dp[idx + 1][1][capacity]);
+                        profit = Math.max(-prices[idx] + prev[0][capacity] , 0 + prev[1][capacity]);
                     }
                     else{
-                        profit = Math.max(prices[idx] + dp[idx + 1][1][capacity - 1] , 0 + dp[idx + 1][0][capacity]);
+                        profit = Math.max(prices[idx] + prev[1][capacity - 1] , 0 + prev[0][capacity]);
                     }
 
-                    dp[idx][option][capacity] = profit;
+                    curr[option][capacity] = profit;
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[0][1][2];
+        return prev[1][2];
     }
 }
