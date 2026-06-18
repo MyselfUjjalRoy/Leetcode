5302ce708e6469db1898aafc624c1edef82b2e66
Ujjal Roy <royu99099@gmail.com>
2026-06-18 14:00:42 +0530

    Time: 0 ms (100%), Space: 43.4 MB (62.81%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,32 +1,19 @@
 class Solution {
-    int n;
-    int[][] dp;
     public int maxProfit(int[] prices) {
-        n = prices.length;
-        dp = new int[n + 1][2];
-        for(int i = 0; i < n + 1; i++){
-            Arrays.fill(dp[i] , -1);
+        int n = prices.length;
+        int[][] dp = new int[n + 2][2];
+
+        for(int idx = n - 1; idx >= 0; idx--){
+            for(int option = 0; option <= 1; option++){
+                if(option == 1){
+                    dp[idx][option] = Math.max(-prices[idx] + dp[idx + 1][0] , 0 + dp[idx + 1][1]);
+                }
+                else{
+                    dp[idx][option] = Math.max(prices[idx] + dp[idx + 2][1] , 0 + dp[idx + 1][0]);
+                }
+            }
         }
 
-        return solve(0 , 1 , prices);
-    }
-
-    public int solve(int idx , int option , int[] prices){
-        if(idx >= n) return 0;
-        
-        if(dp[idx][option] != -1){
-            return dp[idx][option];
-        }
-
-        int profit = 0;
-
-        if(option == 1){
-            profit = Math.max(-prices[idx] + solve(idx + 1 , 0 , prices) , 0 + solve(idx + 1 , 1 , prices));
-        }
-        else{
-            profit = Math.max(prices[idx] + solve(idx + 2 , 1 , prices) , 0 + solve(idx + 1 , 0 , prices));
-        }
-
-        return dp[idx][option] = profit;
+        return dp[0][1];
     }
 }
