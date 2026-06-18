df8705712474d24a0ce289ecbbfe4ea5d4540dcd
Ujjal Roy <royu99099@gmail.com>
2026-06-18 13:01:49 +0530

    Time: 113 ms (20.3%), Space: 148.5 MB (6.72%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,36 @@
 class Solution {
     int n;
     int[][][] dp;
-    public int solve(int idx , int buy , int capacity , int[] prices){
-        if(idx == n) return 0;
-        if(capacity == 0) return 0;
-
-        if(dp[idx][buy][capacity] != -1){
-            return dp[idx][buy][capacity];
-        }
-
-        if(buy == 1){
-            return dp[idx][buy][capacity] = Math.max(-prices[idx] + solve(idx + 1 , 0 , capacity , prices) , solve(idx + 1 , 1 , capacity , prices));
-        }
-        else{
-            return dp[idx][buy][capacity] = Math.max(prices[idx] + solve(idx + 1 , 1 , capacity - 1 , prices) , solve(idx + 1 , 0 , capacity , prices));
-        }
-    }
     public int maxProfit(int[] prices) {
         n = prices.length;
-        dp = new int[n][2][3];
 
+        dp = new int[n + 1][2][3];
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < 2; j++){
+            for(int j = 0; j <= 1; j++){
                 Arrays.fill(dp[i][j] , -1);
             }
         }
 
         return solve(0 , 1 , 2 , prices);
+    }
+
+    public int solve(int idx , int option , int capacity , int[] prices){
+        if(idx == n) return 0;
+        if(capacity == 0) return 0;
+
+        if(dp[idx][option][capacity] != -1){
+            return dp[idx][option][capacity];
+        }
+
+        int profit = 0;
+
+        if(option == 1){
+            profit = Math.max(-prices[idx] + solve(idx + 1 , 0 , capacity , prices) , 0 + solve(idx + 1 , 1 , capacity , prices));
+        }
+        else{
+            profit = Math.max(prices[idx] + solve(idx + 1 , 1 , capacity - 1 , prices) , 0 + solve(idx + 1 , 0 , capacity , prices));
+        }
+
+        return dp[idx][option][capacity] = profit;
     }
 }
