c8fa9fc76ab204126b548957570f27c6b26f87ee
Ujjal Roy <royu99099@gmail.com>
2026-06-19 16:59:39 +0530

    Time: 14 ms (95.55%), Space: 44.6 MB (41.17%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,38 +1,35 @@
-class Solution { // Striver 
-    int[][] dp;
+class Solution {
     public int minCost(int n, int[] cuts) {
         int len = cuts.length;
+
         int[] newCuts = new int[len + 2];
         newCuts[0] = 0;
-        for(int i = 1; i < len + 1; i++){
+        for (int i = 1; i < len + 1; i++) {
             newCuts[i] = cuts[i - 1];
         }
         newCuts[len + 1] = n;
 
         Arrays.sort(newCuts);
 
-        dp = new int[len + 1][len + 1];
-        for(int i = 0; i < len + 1; i++){
-            Arrays.fill(dp[i] , -1);
+        int[][] dp = new int[len + 2][len + 2];
+
+        for (int i = len; i >= 1; i--) {
+            for (int j = 1; j <= len; j++) {
+
+                if(i > j) continue;
+
+                int mini = Integer.MAX_VALUE;
+
+                for (int k = i; k <= j; k++) {
+                    int cost = newCuts[j + 1] - newCuts[i - 1] + dp[i][k - 1] + dp[k + 1][j];
+
+                    mini = Math.min(mini, cost);
+                }
+
+                dp[i][j] = mini;
+            }
         }
 
-        return solve(1 , len , newCuts);
-    }
-
-    public int solve(int i , int j , int[] newCuts){
-        if(i > j) return 0;
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int mini = Integer.MAX_VALUE;
-        for(int k = i; k <= j; k++){
-            int cost = newCuts[j + 1] - newCuts[i - 1] + solve(i , k - 1 , newCuts) + solve(k + 1 , j , newCuts);
-
-            mini = Math.min(mini , cost);
-        }
-
-        return dp[i][j] = mini;
+        return dp[1][len];
     }
 }
