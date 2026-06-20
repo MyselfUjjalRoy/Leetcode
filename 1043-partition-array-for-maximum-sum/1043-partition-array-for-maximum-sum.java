5e3b363e6ee914ec6f6946d29cf3407340d83e62
Ujjal Roy <royu99099@gmail.com>
2026-06-20 17:49:37 +0530

    Time: 7 ms (50.68%), Space: 43.7 MB (90.11%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,33 +1,22 @@
 class Solution {
-    int n;
-    int[] dp;
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        n = arr.length;
-        dp = new int[n];
-        // dp[i] -> give me the max sum if we have the array starting from i
-        Arrays.fill(dp , -1);
+        int n = arr.length;
+        int[] dp = new int[n + 1];
 
-        return solve(0 , k , arr);
-    }
-    public int solve(int idx , int k , int[] arr){
-        if(idx == n){
-            return 0;
+        for (int idx = n - 1; idx >= 0; idx--) {
+            int maxElement = Integer.MIN_VALUE;
+            int len = 0;
+            int ans = Integer.MIN_VALUE;
+
+            for (int i = idx; i < Math.min(n, idx + k); i++) {
+                len++;
+                maxElement = Math.max(maxElement, arr[i]);
+                ans = Math.max(ans, len * maxElement + dp[i + 1]);
+            }
+
+            dp[idx] = ans;
         }
 
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-
-        int maxElement = Integer.MIN_VALUE;
-        int len = 0;
-        int ans = Integer.MIN_VALUE;
-
-        for(int i = idx; i < Math.min(n , idx + k); i++){
-            len++;
-            maxElement = Math.max(maxElement , arr[i]);
-            ans = Math.max(ans , len * maxElement + solve(i + 1 , k , arr));
-        }
-
-        return dp[idx] = ans;
+        return dp[0];
     }
 }
