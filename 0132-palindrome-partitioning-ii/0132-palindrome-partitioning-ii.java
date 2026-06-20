2467a54a239951ba065149212c36798d7789bad3
Ujjal Roy <royu99099@gmail.com>
2026-06-20 16:59:35 +0530

    Time: 38 ms (52.27%), Space: 47.7 MB (22.84%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,8 +1,5 @@
 class Solution {
-    /*
-    Best Example to dry run -> abcb
-    */
-    public int solve(String s) {
+    public int minCut(String s) {
         int n = s.length();
         boolean[][] dp = new boolean[n + 1][n + 1];
 
@@ -10,14 +7,14 @@ class Solution {
             for(int i = 0; i + length - 1 < n; i++){
                 int j = i + length - 1;
 
-                if(i == j){ // length 1 always pallindrome
+                if(i == j){ // length = 1
                     dp[i][j] = true;
                 }
-                else if(i + 1 == j){ // length 2
+                else if(i + 1 == j){
                     dp[i][j] = (s.charAt(i) == s.charAt(j));
                 }
                 else{
-                    dp[i][j] = (s.charAt(i) == s.charAt(j)) && (dp[i + 1][j - 1] == true);
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && (dp[i + 1][j - 1] == true));
                 }
             }
         }
@@ -26,11 +23,11 @@ class Solution {
         Arrays.fill(cuts , Integer.MAX_VALUE);
 
         for(int i = 0; i < n; i++){
-            if(dp[0][i]){ // means pallindrome
+            if(dp[0][i]){
                 cuts[i] = 0;
             }
             else{
-                for(int k = 0; k < i; k++){ // means k pe cut , and cut ke baad pallindrome hona parega and also agar i th mei min hai after cut , then update
+                for(int k = 0; k < i; k++){
                     if(dp[k + 1][i] == true && 1 + cuts[k] < cuts[i]){
                         cuts[i] = 1 + cuts[k];
                     }
@@ -39,15 +36,5 @@ class Solution {
         }
 
         return cuts[n - 1];
-    }
-
-    public int minCut(String s){
-        int n = s.length();
-
-        // if(n == 0 || n == 1){
-        //     return 0;
-        // }
-
-        return solve(s);
     }
 }
