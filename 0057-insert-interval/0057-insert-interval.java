4031952839f94c52b5c6079ae77d049b366786ee
Ujjal Roy <royu99099@gmail.com>
2026-06-22 01:00:46 +0530

    Time: 0 ms (100%), Space: 46.7 MB (94.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,31 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new ArrayList<>();
+        int n = intervals.length;
+        List<int[]> list = new ArrayList<>();
 
         int i = 0;
-        int n = intervals.length;
-
         while(i < n && intervals[i][1] < newInterval[0]){
-            result.add(intervals[i]);
-            i++;
-        }   
-        while(i < n && intervals[i][0] <= newInterval[1]){
-            newInterval[0] = Math.min(intervals[i][0] , newInterval[0]);
-            newInterval[1] = Math.max(intervals[i][1] , newInterval[1]);
+            list.add(intervals[i]);
             i++;
         }
-        result.add(newInterval);
+
+        while(i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0] , intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1] , intervals[i][1]);
+            i++;
+        }
+        list.add(newInterval);
 
         while(i < n){
-            result.add(intervals[i]);
+            list.add(intervals[i]);
             i++;
         }
-        return result.toArray(new int[result.size()][]);
+
+        int[][] ans = new int[list.size()][2];
+        for(int k = 0; k < list.size(); k++){
+            ans[k] = list.get(k);
+        }
+
+        return ans;
     }
 }
