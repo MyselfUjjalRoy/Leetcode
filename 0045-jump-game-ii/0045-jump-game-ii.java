4e5c965ffb177631c9f6dfee95ca5254b227b38d
Ujjal Roy <royu99099@gmail.com>
2026-06-22 00:28:57 +0530

    Time: 1 ms (99.71%), Space: 47.4 MB (21.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,20 @@
-//from striver
 class Solution {
     public int jump(int[] nums) {
-        int l = 0 , r = 0;
         int n = nums.length;
+        int l = 0 , r = 0;
+
         int jumps = 0;
 
         while(r < n - 1){
             int farthest = 0;
-            for(int index = l; index <= r; index++){
-                farthest = Math.max(farthest , index + nums[index]);
+            for(int idx = l; idx <= r; idx++){
+                farthest = Math.max(farthest , idx + nums[idx]);
             }
             l = r + 1;
             r = farthest;
             jumps++;
         }
+
         return jumps;
     }
 }
