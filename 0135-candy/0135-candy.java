39178a575fd771ba2f0125822eaea58a85d4aa23
Ujjal Roy <royu99099@gmail.com>
2026-06-22 12:38:24 +0530

    Time: 3 ms (89.1%), Space: 47.9 MB (6.94%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,31 +1,36 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int sum = 1;
-        int i = 1;
+        
+        int[] left = new int[n];
+        int[] right = new int[n];
 
-        while(i < n){
-            if(ratings[i] == ratings[i - 1]){
-                sum = sum + 1;
-                i++;
+        left[0] = 1;
+        right[n - 1] = 1;
+
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1]){
+                left[i] = left[i - 1] + 1;
             }
-            int peak = 1;
-            while( i < n && ratings[i] > ratings[i - 1]){
-                peak++;
-                sum += peak;
-                i++;
-            }
-            int down = 1;
-            while( i < n && ratings[i] < ratings[i - 1]){
-                sum += down;
-                down++;
-                i++;
-            }
-            if(down > peak){
-                sum += down - peak;
+            else{
+                left[i] = 1;
             }
         }
-        return sum;
-        
+
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                right[i] = right[i + 1] + 1;
+            }
+            else{
+                right[i] = 1;
+            }
+        }
+
+        int candy = 0;
+        for(int i = 0; i < n; i++){
+            candy += Math.max(left[i] , right[i]);
+        }
+
+        return candy;
     }
 }
