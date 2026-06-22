745f0421a865c0e788b3f1cb88ef217e3ffcb70d
Ujjal Roy <royu99099@gmail.com>
2026-06-22 16:07:21 +0530

    Time: 0 ms (100%), Space: 48.1 MB (49.96%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = {findFirst(nums , target) , findLast(nums , target)};
-        return ans;
-    }
-    public int findFirst(int[] nums , int target){
-        int low = 0, high = nums.length - 1;
-        int firstPosition = -1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target){
-                firstPosition = mid;
-                high = mid -1;
-            }
-            else if(nums[mid] < target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }            
-        }
-        return firstPosition;
+        int first = firstOccurrence(nums , target);
+        int last = lastOccurrence(nums , target);
+
+        return new int[]{first , last};
     }
 
-     public int findLast(int[] nums , int target){
-        int low = 0, high = nums.length - 1;
-        int lastPosition = -1;
+    public int firstOccurrence(int[] nums , int target){
+        int low = 0;
+        int high = nums.length - 1;
+
+        int ans = -1;
+
         while(low <= high){
-            int mid = low + (high - low)/2;
+            int mid = low + (high - low) / 2;
+
             if(nums[mid] == target){
-                lastPosition = mid;
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int lastOccurrence(int[] nums , int target){
+        int low = 0;
+        int high = nums.length - 1;
+
+        int ans = -1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target){
+                ans = mid;
                 low = mid + 1;
             }
             else if(nums[mid] < target){
@@ -36,8 +48,9 @@ class Solution {
             }
             else{
                 high = mid - 1;
-            }            
+            }
         }
-        return lastPosition;
+
+        return ans;
     }
 }
