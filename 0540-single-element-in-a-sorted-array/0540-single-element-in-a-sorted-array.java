f9f37cf96060529a2df9f708dffc7138888e3e7b
Ujjal Roy <royu99099@gmail.com>
2026-06-22 17:52:24 +0530

    Time: 0 ms (100%), Space: 52.5 MB (96.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,36 +1,39 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
 
-        if (nums.length == 1)
-            return nums[0];
-        else if (nums[0] != nums[1])
-            return nums[0];
-        else if (nums[nums.length - 1] != nums[nums.length - 2])
-            return nums[nums.length - 1];
+        if(n == 1) return nums[0];
+        else if(nums[0] != nums[1]) return nums[0];
+        else if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
 
-        int start = 1;
-        int end = nums.length - 2;
+        int low = 1;
+        int high = n - 2;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
 
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
                 return nums[mid];
-            } else if (mid % 2 == 1) { // if mid index is odd
-                if (nums[mid - 1] == nums[mid]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
+            }
+
+            if(mid % 2 == 1){
+                if(nums[mid] == nums[mid - 1]){
+                    low = mid + 1;
                 }
-            } else { // if mid index is even
-                if (nums[mid] == nums[mid + 1]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
+                else{
+                    high = mid - 1;
+                }
+            }
+            else{
+                if(nums[mid] == nums[mid + 1]){
+                    low = mid + 2;
+                }
+                else{
+                    high = mid - 1;
                 }
             }
         }
 
-        return -1;
+        return -1; // unreachable
     }
 }
