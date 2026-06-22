0b87efade65bf9beadb3063c1995d5928bc5b166
Ujjal Roy <royu99099@gmail.com>
2026-06-22 12:50:55 +0530

    Time: 4 ms (23.39%), Space: 47.6 MB (67.98%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -3,10 +3,9 @@ class Solution {
         int n = ratings.length;
         
         int[] left = new int[n];
-        int[] right = new int[n];
-
         left[0] = 1;
-        right[n - 1] = 1;
+
+        int candy = 0;
 
         for(int i = 1; i < n; i++){
             if(ratings[i] > ratings[i - 1]){
@@ -17,18 +16,20 @@ class Solution {
             }
         }
 
+        candy = Math.max(1 , left[n - 1]);
+
+        int curr = 1;
+        int right = 1;
+
         for(int i = n - 2; i >= 0; i--){
             if(ratings[i] > ratings[i + 1]){
-                right[i] = right[i + 1] + 1;
+                right = right + 1;
             }
             else{
-                right[i] = 1;
+                right = 1;
             }
-        }
 
-        int candy = 0;
-        for(int i = 0; i < n; i++){
-            candy += Math.max(left[i] , right[i]);
+            candy += Math.max(left[i] , right);
         }
 
         return candy;
