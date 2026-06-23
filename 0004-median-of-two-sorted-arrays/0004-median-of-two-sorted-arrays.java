86f130975091adf0a24f422b3340ad7ad82eedd9
Ujjal Roy <royu99099@gmail.com>
2026-06-23 17:35:00 +0530

    Time: 1 ms (100%), Space: 49.1 MB (15.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,44 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    
-     if(nums1.length>nums2.length)
-     {
-         return findMedianSortedArrays(nums2,nums1);
-     }  
-     int n1=nums1.length;
-     int n2=nums2.length; 
-     int N=n1+n2;
-     int start=0;
-     int end=n1;
-     while(start<=end)
-     {
-            int cut1=start+(end-start)/2;
-            int cut2=N/2-cut1;
-        
-            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
-            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
-        if(l1<=r2 && l2<=r1)
-        {
-            if(N%2 == 0)
-            {
-                  return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        if (n2 < n1)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int low = 0;
+        int high = n1;
+
+        while (low <= high) {
+            int cut1 = low + (high - low) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int left2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int right1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
+            int right2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                }
+
+                else {
+                    return Math.max(left1, left2);
+                }
+            } else if (left1 > left2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
             }
-            else
-            {
-              return  Math.min(r1,r2);
-            }
         }
-        else if(l1>r2)
-        {
-            end=cut1-1;
-        }
-        else
-        {
-            start=cut1+1;
-        }
-     }
-     return 0.0;
+
+        return -1; // unreachable
     }
 }
