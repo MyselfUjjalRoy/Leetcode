387d23f911393a5f26d52e69c58d8a1ed653ae92
Ujjal Roy <royu99099@gmail.com>
2026-06-23 13:52:32 +0530

    Time: 0 ms (100%), Space: 44.6 MB (39.57%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,9 +1,10 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] <= k){
+            if(arr[i] <= k){ // all the elements lesser than k , will take the place
                 k++;
             }
+            else break;
         }
 
         return k;
