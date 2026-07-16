2ff581cf013a180e431680822497f9400d3daa18
Ujjal Roy <royu99099@gmail.com>
2026-07-16 23:10:50 +0530

    Time: 0 ms (100%), Space: 43.4 MB (64.34%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,21 +5,22 @@ class Solution {
 
         int[][] dp = new int[m][n];
 
-        dp[0][0] = grid[0][0] == 1 ? 0 : 1;
+        dp[0][0] = grid[0][0] == 0 ? 1 : 0;
 
-        for (int col = 1; col < n; col++) {
+        for(int col = 1; col < n; col++){
             dp[0][col] = grid[0][col] == 1 ? 0 : dp[0][col - 1];
         }
 
-        for (int row = 1; row < m; row++) {
+        for(int row = 1; row < m; row++){
             dp[row][0] = grid[row][0] == 1 ? 0 : dp[row - 1][0];
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (grid[i][j] == 1) {
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(grid[i][j] == 1){
                     dp[i][j] = 0;
-                } else {
+                }
+                else{
                     int left = dp[i][j - 1];
                     int up = dp[i - 1][j];
 
