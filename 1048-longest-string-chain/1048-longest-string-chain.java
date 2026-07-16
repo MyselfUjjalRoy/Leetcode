2ada10aaa66d7d2c149a685d5e495bcc7750eadf
Ujjal Roy <royu99099@gmail.com>
2026-07-16 16:07:29 +0530

    Time: 48 ms (62.27%), Space: 46.5 MB (45.18%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,21 +1,17 @@
 class Solution {
     public int longestStrChain(String[] words) {
         int n = words.length;
-        
         Arrays.sort(words , (a , b) -> a.length() - b.length());
 
+        int maxLen = 1;
         int[] dp = new int[n];
         Arrays.fill(dp , 1);
-
-        int maxLen = 1;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < i; j++){
                 if(isPredecessor(words[j] , words[i])){
-                    if(dp[i] < 1 + dp[j]){
-                        dp[i] = 1 + dp[j];
-                        maxLen = Math.max(maxLen , dp[i]);
-                    }
+                    dp[i] = Math.max(dp[i] , 1 + dp[j]);
+                    maxLen = Math.max(maxLen , dp[i]);
                 }
             }
         }
@@ -23,16 +19,16 @@ class Solution {
         return maxLen;
     }
 
-    public boolean isPredecessor(String prev , String curr){
-        int m = prev.length();
-        int n = curr.length();
+    public boolean isPredecessor(String word1 , String word2){
+        int m = word1.length();
+        int n = word2.length();
 
         if(n - m != 1) return false;
 
         int i = 0 , j = 0;
 
         while(i < m && j < n){
-            if(prev.charAt(i) == curr.charAt(j)){
+            if(word1.charAt(i) == word2.charAt(j)){
                 i++;
             }
             j++;
