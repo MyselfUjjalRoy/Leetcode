60662987bbb2ac0401982425ed938ebbca4d5f78
Ujjal Roy <royu99099@gmail.com>
2026-07-16 19:45:11 +0530

    Time: 3 ms (98.39%), Space: 47 MB (71.82%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,30 +1,38 @@
 class Solution {
+    int m , n;
+    int[][] dp;
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
+        m = word1.length();
+        n = word2.length();
 
-        int[] prev = new int[n + 1];
-
+        dp = new int[m + 1][n + 1];
         for(int i = 0; i < m + 1; i++){
-            int[] curr = new int[n + 1];
-            for(int j = 0; j < n + 1; j++){
-                if(i == 0 || j == 0){
-                    curr[j] = i + j;
-                }
-                else if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    curr[j] = prev[j - 1];
-                }
-                else{
-                    int insert = curr[j - 1] + 1;
-                    int delete = prev[j] + 1;
-                    int replace = prev[j - 1] + 1;
-
-                    curr[j] = Math.min(insert , Math.min(delete , replace));
-                }
-            }
-            prev = curr;
+            Arrays.fill(dp[i] , -1);
         }
 
-        return prev[n];
+        return solve(word1 , word2 , m , n);
+    }
+
+    public int solve(String word1 , String word2 , int i , int j){
+        if(i == 0 || j == 0){
+            return i + j;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+            dp[i][j] = solve(word1 , word2 , i - 1 , j - 1);
+        }
+        else{
+            int delete = 1 + solve(word1 , word2 , i - 1 , j);
+            int insert = 1 + solve(word1 , word2 , i , j - 1);
+            int update = 1 + solve(word1 , word2 , i - 1 , j - 1);
+
+            dp[i][j] = Math.min(insert , Math.min(delete , update));
+        }
+
+        return dp[i][j];
     }
 }
