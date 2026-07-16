5629e4e4d0550564e9ca3132c32ba0c0514e2d06
Ujjal Roy <royu99099@gmail.com>
2026-07-16 15:50:02 +0530

    Time: 166 ms (5.14%), Space: 65.9 MB (9.18%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,22 +1,37 @@
 class Solution {
+    int n;
+    int[][] dp;
     public int findLongestChain(int[][] pairs) {
+        n = pairs.length;
         Arrays.sort(pairs , (a , b) -> a[0] - b[0]);
-
-        int n = pairs.length;
-        int maxLen = 1;
-
-        int[] dp = new int[n];
-        Arrays.fill(dp , 1);
-
+        dp = new int[n][n];
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(pairs[j][1] < pairs[i][0]){
-                    dp[i] = Math.max(dp[i] , 1 + dp[j]);
-                    maxLen = Math.max(maxLen , dp[i]);
-                }
-            }
+            Arrays.fill(dp[i] , -1);
         }
 
-        return maxLen;
+        return solve(-1 , 0 , pairs);
+    }
+
+    public int solve(int prevIdx , int currIdx , int[][] pairs){
+        if(currIdx >= n) return 0;
+
+        if(prevIdx != -1 && dp[prevIdx][currIdx] != -1){
+            return dp[prevIdx][currIdx];
+        }
+
+        int skip = solve(prevIdx , currIdx + 1 , pairs);
+        
+        int take = 0;
+        if(prevIdx == -1 || pairs[prevIdx][1] < pairs[currIdx][0]){
+            take = 1 + solve(currIdx , currIdx + 1 , pairs);
+        }
+
+        int ans = Math.max(take , skip);
+
+        if(prevIdx != -1){
+            dp[prevIdx][currIdx] = ans;
+        }
+
+        return ans;
     }
 }
