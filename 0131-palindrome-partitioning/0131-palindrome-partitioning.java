d535210a62d911f40fb62c1ffd42e7396c16180f
Ujjal Roy <royu99099@gmail.com>
2026-07-16 22:22:51 +0530

    Time: 7 ms (98.53%), Space: 65.3 MB (57.66%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,42 +1,39 @@
 class Solution {
+    boolean[][] dp;
     public List<List<String>> partition(String s) {
         int n = s.length();
 
-        boolean[][] dp = new boolean[n + 1][n + 1];
-        // dp[i][j] -> true : s[i ... j] is pallindrome
+        dp = new boolean[n + 1][n + 1];
 
-        for (int length = 1; length <= n; length++) {
-            for (int i = 0; i + length - 1 < n; i++) {
+        for(int length = 1; length <= n; length++){
+            for(int i = 0; i + length - 1 < n; i++){
                 int j = i + length - 1;
-                if (i == j) { // length 1
-                    dp[i][j] = true;
-                } else if (i + 1 == j) { // length 2
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
-                } else {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j)) && dp[i + 1][j - 1];
-                }
+                
+                if(i == j) dp[i][j] = true;
+                else if(i + 1 == j) dp[i][j] = (s.charAt(i) == s.charAt(j));
+                else dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
             }
         }
 
-        List<List<String>> ans = new ArrayList<>();
         List<String> currPartition = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
 
-        solve(0, s, dp, currPartition, ans);
+        solve(0 , s , currPartition , ans);
 
         return ans;
     }
 
-    public void solve(int i, String s, boolean[][] dp, List<String> currPartition, List<List<String>> ans) {
-        if (i == s.length()) {
+    public void solve(int i , String s , List<String> currPartition , List<List<String>> ans){
+        int n = s.length();
+        if(i == n){
             ans.add(new ArrayList<>(currPartition));
             return;
         }
 
-        for (int j = i; j < s.length(); j++) {
-            if (dp[i][j] == true) { // means pallindrome
-                currPartition.add(s.substring(i, j + 1));
-                solve(j + 1, s, dp, currPartition, ans);
-
+        for(int j = i; j < n; j++){
+            if(dp[i][j]){
+                currPartition.add(s.substring(i , j + 1));
+                solve(j + 1 , s , currPartition , ans);
                 currPartition.remove(currPartition.size() - 1);
             }
         }
