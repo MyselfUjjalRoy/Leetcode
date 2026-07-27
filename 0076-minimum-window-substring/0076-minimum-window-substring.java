791439b01d9f49198f058c9b0c36368c22cf1004
Ujjal Roy <royu99099@gmail.com>
2026-07-28 02:45:55 +0530

    Time: 20 ms (38.36%), Space: 47.8 MB (24.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
+
         Map<Character , Integer> need = new HashMap<>();
 
         for(char ch : t.toCharArray()){
             need.put(ch , need.getOrDefault(ch , 0) + 1);
         }
+        
+        int required = need.size();
 
-        Map<Character , Integer> window = new HashMap<>();
-        int have = 0;
         int left = 0;
-
-        int start = 0; // to get the string
+        int start = 0;
+        int have = 0;
         int minLen = Integer.MAX_VALUE;
+        Map<Character , Integer> window = new HashMap<>();
 
         for(int right = 0; right < n; right++){
             char ch = s.charAt(right);
             window.put(ch , window.getOrDefault(ch , 0) + 1);
 
-            if(need.containsKey(ch) && need.get(ch).equals(window.get(ch))){
+            if(need.containsKey(ch) && window.get(ch).equals(need.get(ch))){ // not == 
                 have++;
             }
 
-            while(have == need.size()){
+            while(have == required){
                 if(right - left + 1 < minLen){
-                    minLen = Math.min(minLen , right - left + 1);
+                    minLen = right - left + 1;
                     start = left;
                 }
 
                 char leftChar = s.charAt(left);
-                window.put(leftChar , window.get(leftChar) - 1);
-                
+                window.put(leftChar , window.getOrDefault(leftChar , 0) - 1);
+
                 if(need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)){
                     have--;
                 }
-
                 left++;
             }
         }
