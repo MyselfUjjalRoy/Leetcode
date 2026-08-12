4f1575b621d18a431a92fcd34518193e2ce536bd
Ujjal Roy <royu99099@gmail.com>
2026-08-13 00:15:35 +0530

    Time: 75 ms (26.02%), Space: 88.8 MB (64.18%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -5,17 +5,23 @@ class Solution {
         int j = 0;
         int ans = 0;
         int n = nums.length;
+        int culprit = 0;
         while(j < n){
             map.put(nums[j] , map.getOrDefault(nums[j] , 0) + 1);
-            while(i < j && map.get(nums[j]) > k){
+           
+            if(map.get(nums[j]) == k + 1){
+                culprit++;
+            }
+            if(culprit > 0){
                 map.put(nums[i] , map.get(nums[i]) - 1);
-                
-                if(map.get(nums[i]) == 0){
-                    map.remove(nums[i]);
+                if(map.get(nums[i]) == k){
+                    culprit--;
                 }
                 i++;
             }
-            ans = Math.max(ans , j - i + 1);
+            if(culprit == 0){
+                ans = Math.max(ans , j - i + 1);
+            }
             j++;
         }
         return ans;
