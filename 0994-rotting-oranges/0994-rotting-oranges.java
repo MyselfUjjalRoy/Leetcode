0d347f45eca5482120c95fec3077e992706e88b6
Ujjal Roy <royu99099@gmail.com>
2026-08-13 09:39:52 +0530

    Time: 2 ms (89.7%), Space: 43.9 MB (85.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -4,15 +4,12 @@ class Solution {
         int n = grid[0].length;
 
         int fresh = 0;
-
         Queue<int[]> queue = new LinkedList<>();
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1) fresh++;
-                else if(grid[i][j] == 2){
-                    queue.offer(new int[]{i , j});
-                }
+                else if(grid[i][j] == 2) queue.offer(new int[]{i , j});
             }
         }
 
