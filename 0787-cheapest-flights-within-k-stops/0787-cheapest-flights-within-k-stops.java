63cf14ea784d40ebd82f70ea4452257c513d08ca
Ujjal Roy <royu99099@gmail.com>
2026-08-14 11:14:56 +0530

    Time: 7 ms (41.43%), Space: 46.7 MB (25.55%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,44 +1,43 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
-        }
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
         for(int[] flight : flights){
-            int from = flight[0];
-            int to = flight[1];
-            int cost = flight[2];
+            int u = flight[0];
+            int v = flight[1];
+            int wt = flight[2];
 
-            adj.get(from).add(new int[]{to , cost});
+            adj.get(u).add(new int[]{v , wt});
         }
 
-        Queue<int[]> queue = new LinkedList<>();
         int[] dist = new int[n];
         Arrays.fill(dist , Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a , b) -> Integer.compare(a[0] , b[0]));
+        pq.offer(new int[]{0 , src , 0}); // [stops , node , cost]
+
         dist[src] = 0;
 
-        queue.offer(new int[]{src , 0 , 0}); //[city , cost , stops]
-
-        while(!queue.isEmpty()){
-            int[] curr = queue.poll();
-            int city = curr[0];
-            int cost = curr[1];
-            int stops = curr[2];
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int stops = curr[0];
+            int u = curr[1];
+            int cost = curr[2];
 
             if(stops > k) continue;
 
-            for(int[] nei : adj.get(city)){
-                int nextCity = nei[0];
-                int price = nei[1];
-                int totalPrice = cost + price;
-                
-                if(totalPrice < dist[nextCity]){
-                    dist[nextCity] = totalPrice;
-                    queue.offer(new int[]{nextCity , totalPrice , stops + 1});
+            for(int[] nei : adj.get(u)){
+                int v = nei[0];
+                int wt = nei[1];
+
+                if(cost + wt < dist[v] && stops <= k){
+                    dist[v] = cost + wt;
+                    pq.offer(new int[]{stops + 1 , v , dist[v]});
                 }
             }
         }
+
         return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }
