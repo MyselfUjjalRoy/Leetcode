d193eaf1bd88936480ea3b4861541e21ad77ba51
Ujjal Roy <royu99099@gmail.com>
2026-08-14 11:39:33 +0530

    Time: 14 ms (36.35%), Space: 51.8 MB (66.29%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,18 +1,16 @@
 class Solution {
+    int MOD = (int)(1e9 + 7);
     public int countPaths(int n, int[][] roads) {
-        int MOD = (int)1e9 + 7;
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
-        }
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
         for(int[] road : roads){
             int u = road[0];
             int v = road[1];
-            int wt = road[2];
+            int time = road[2];
 
-            adj.get(u).add(new int[]{v , wt});
-            adj.get(v).add(new int[]{u , wt});
+            adj.get(u).add(new int[]{v , time});
+            adj.get(v).add(new int[]{u , time});
         }
 
         long[] dist = new long[n];
@@ -23,28 +21,28 @@ class Solution {
         ways[0] = 1;
 
         PriorityQueue<long[]> pq = new PriorityQueue<>((a , b) -> Long.compare(a[0] , b[0]));
-        pq.offer(new long[]{0 , 0}); // [time , node];
+        pq.offer(new long[]{0 , 0}); // [time , node]
 
         while(!pq.isEmpty()){
             long[] curr = pq.poll();
             long time = curr[0];
-            int node = (int)curr[1];
+            int u = (int)curr[1];
 
-            for(int[] nei : adj.get(node)){
-                int nextNode = nei[0];
-                int nextTime = nei[1];
+            for(int[] nei : adj.get(u)){
+                int v = nei[0];
+                int wt = nei[1];
 
-                if(time + nextTime < dist[nextNode]){
-                    dist[nextNode] = time + nextTime;
-                    ways[nextNode] = ways[node];
-                    pq.offer(new long[]{dist[nextNode] , nextNode});
+                if(time + wt < dist[v]){
+                    dist[v] = time + wt;
+                    ways[v] = ways[u];
+                    pq.offer(new long[]{dist[v] , v});
                 }
-                else if(time + nextTime == dist[nextNode]){
-                    ways[nextNode] = (ways[nextNode] + ways[node]) % MOD;
+                else if(time + wt == dist[v]){
+                    ways[v] = (ways[u] + ways[v]) % MOD;
                 }
             }
         }
 
-       return ways[n - 1];
+        return ways[n - 1];
     }
 }
