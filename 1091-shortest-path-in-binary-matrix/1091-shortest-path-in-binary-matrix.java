4e7ca56f5a0be789d7eb2bac90488a8a8cb013f0
Ujjal Roy <royu99099@gmail.com>
2026-08-14 10:24:23 +0530

    Time: 17 ms (42.51%), Space: 47.3 MB (86.53%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,21 +2,15 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
 
-        if(grid[0][0] != 0 || grid[n - 1][n - 1] != 0){
-            return -1;
-        }
-
-        //if(n == 1 && grid[0][0] == 0) return 1;
+        if(grid[0][0] != 0 || grid[n - 1][n - 1] != 0) return -1;
 
         boolean[][] visited = new boolean[n][n];
 
         Queue<int[]> queue = new LinkedList<>();
-
         queue.offer(new int[]{0 , 0 , 1});
-        visited[0][0] = true;
 
-        int[] dir = {-1 , 0 , -1 , 1 , 0 , 1 , 1 , 1 , 1 , 0 , 1 , -1 , 0 , -1 , -1 , -1};
-        
+        int[] dir = {-1 , 0 , 1 , 0 , 0 , -1 , 0 , 1 , -1 , -1 , -1 , 1 , 1 , 1 , 1 , -1};
+
         while(!queue.isEmpty()){
             int[] curr = queue.poll();
             int r = curr[0];
@@ -24,14 +18,14 @@ class Solution {
             int dist = curr[2];
 
             if(r == n - 1 && c == n - 1) return dist;
-            for(int d = 0; d < 8; d++){
+            
+            for(int d  = 0; d < 8; d++){
                 int nR = r + dir[2 * d];
                 int nC = c + dir[2 * d + 1];
 
                 if(nR >= 0 && nC >= 0 && nR < n && nC < n && !visited[nR][nC] && grid[nR][nC] == 0){
-                    //if(nR == n - 1 && nC == n - 1) return dist + 1;
-                    queue.offer(new int[]{nR , nC , dist + 1});
                     visited[nR][nC] = true;
+                    queue.offer(new int[]{nR , nC , dist + 1});
                 }
             }
         }
