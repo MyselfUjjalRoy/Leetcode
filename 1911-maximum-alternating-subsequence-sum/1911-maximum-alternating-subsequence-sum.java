b9907728109fa869dd66d3baa067bb75cc00afbf
Ujjal Roy <royu99099@gmail.com>
2026-09-07 11:05:42 +0530

    Time: 53 ms (54.24%), Space: 120.4 MB (48.2%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,30 +1,34 @@
 class Solution {
     long[][] dp;
     int n;
-    public long solve(int idx , int[] nums , boolean flag){
-        if(idx >= n) return 0;
-
-        int f = flag ? 1 : 0;
-        if(dp[idx][f] != -1) return dp[idx][f];
-
-        long skip = solve(idx + 1 , nums , flag);
-        
-        int val = nums[idx];
-
-        if(!flag) val = -val;
-
-        long take = val + solve(idx + 1 , nums , !flag);
-
-        return dp[idx][f] = Math.max(take ,skip);
-    }
     public long maxAlternatingSum(int[] nums) {
         n = nums.length;
+        dp = new long[n][2];
 
-        dp = new long[n + 1][2]; // 1 based indexing mei change kar liya
         for(int i = 0; i < n; i++){
             Arrays.fill(dp[i] , -1);
         }
 
-        return solve(0 , nums , true);
+        return solve(0 , true , nums);
+    }
+
+    public long solve(int idx , boolean flag , int[] nums){
+        if(idx >= n) return 0;
+
+        int f = flag ? 1 : 0;
+
+        if(dp[idx][f] != -1) return dp[idx][f];
+
+        long skip = solve(idx + 1 , flag , nums);
+
+        long val = nums[idx];
+
+        if(!flag){
+            val = -val;
+        }
+
+        long take = solve(idx + 1 , !flag , nums) + val;
+
+        return dp[idx][f] = Math.max(take , skip);
     }
 }
