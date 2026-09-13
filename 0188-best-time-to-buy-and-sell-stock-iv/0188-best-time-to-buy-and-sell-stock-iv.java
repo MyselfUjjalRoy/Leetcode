b4c1df1e36c0f36670f0e2044b5c085baee92079
Ujjal Roy <royu99099@gmail.com>
2026-09-13 19:42:55 +0530

    Time: 8 ms (29.14%), Space: 45.4 MB (37.61%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,40 +1,31 @@
 class Solution {
+    int n;
+    int[][][] dp;
     public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-        int[][] prev = new int[3][k + 1];
-
-        for(int i = 0; i < n + 1; i++){
-            for(int option = 0; option <= 2; option++){
-                prev[option][0] = 0;
+        n = prices.length;
+        dp = new int[n][2][k + 1];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < 2; j++){
+                Arrays.fill(dp[i][j] , -1);
             }
         }
 
-        for(int option = 0; option <= 2; option++){
-            for(int capacity = 0; capacity < k + 1; capacity++){
-                prev[option][capacity] = 0;
-            }
+        return solve(0 , 1 , k , prices);
+    }
+
+    public int solve(int idx , int option , int k , int[] prices){
+        if(idx == n || k == 0) return 0;
+        if(dp[idx][option][k] != -1) return dp[idx][option][k];
+
+        int profit = 0;
+
+        if(option == 1){
+            profit = Math.max(-prices[idx] + solve(idx + 1 , 0 , k , prices) , 0 + solve(idx + 1 , 1 , k , prices));
+        }
+        else{
+            profit = Math.max(prices[idx] + solve(idx + 1 , 1 , k - 1 , prices) , solve(idx + 1 , 0 , k , prices));
         }
 
-        for(int idx = n - 1; idx >= 0; idx--){
-            int[][] curr = new int[3][k + 1];
-            for(int option = 0; option <= 1; option++){
-                for(int capacity = 1; capacity <= k; capacity++){
-                    int profit = 0;
-
-                    if(option == 1){
-                        profit = Math.max(-prices[idx] + prev[0][capacity] , 0 + prev[1][capacity]);
-                    }
-                    else{
-                        profit = Math.max(prices[idx] + prev[1][capacity - 1] , 0 + prev[0][capacity]);
-                    }
-
-                    curr[option][capacity] = profit;
-                }
-            }
-
-            prev = curr;
-        }
-
-        return prev[1][k];
+        return dp[idx][option][k] = profit;
     }
 }
